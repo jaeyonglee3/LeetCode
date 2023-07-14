932b83486cc433eced799e7f994466425d36f4ed
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2023-07-13 21:11:45 -0400

    Time: 82 ms (63.74%), Space: 17.2 MB (34.27%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -4,4 +4,4 @@ class Solution:
 
 # The maximum digit in the string n decides how many 1's will be present.
 # The total number of 1's required to sum up to form the max digit 
-# is deciding how many numbesr required for the total sum. 
+# is deciding how many numbers required for the total sum. 
