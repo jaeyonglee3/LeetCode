4268f9d06efe57cc019ead086a876dcc1692ed71
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2023-11-14 23:53:14 -0500

    Time: 385 ms (78.73%), Space: 16.4 MB (27.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # variable sized sliding window approach
+        # INCORRECT - variable sized sliding window approach
         # l, r start at index 0
         # while right is still in range
         # if string in window not palindrome or length 1, increment right
@@ -20,15 +20,38 @@ class Solution:
         
         # return res
 
-        if len(s) == 1:
-            return s
+        # CORRECT - brute force:
+        # if len(s) == 1:
+        #     return s
         
-        res = ""
+        # res = ""
 
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                substring = s[i : j + 1]
-                if substring == substring[::-1]:
-                    res = substring if len(substring) > len(res) else res
+        # for i in range(len(s)):
+        #     for j in range(i, len(s)):
+        #         substring = s[i : j + 1]
+        #         if substring == substring[::-1]:
+        #             res = substring if len(substring) > len(res) else res
         
-        return res
+        # return res
+        if len(s) <= 1:
+            return s
+
+        def expand_from_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            
+            return s[left + 1:right]
+
+        max_str = s[0]
+
+        for i in range(len(s) - 1):
+            odd = expand_from_center(i, i)
+            even = expand_from_center(i, i + 1)
+
+            if len(odd) > len(max_str):
+                max_str = odd
+            if len(even) > len(max_str):
+                max_str = even
+
+        return max_str
