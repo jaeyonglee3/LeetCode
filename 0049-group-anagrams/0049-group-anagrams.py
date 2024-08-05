b537703adb3723f3b0c485a26112ab4bf63466ef
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-05 17:44:17 -0400

    Time: 103 ms (16.6%), Space: 21.4 MB (29.8%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,13 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hmap = {}
-        res = []
+        hmap = defaultdict(list)
 
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            if sorted_word in hmap:
-                hmap[sorted_word].append(word)
-            else:
-                hmap[sorted_word] = [word]
-        
-        for key in hmap:
-            res.append(hmap[key])
-        
-        return res
+            char_count = [0] * 26
+            for char in word:
+                char_count[ord(char) - ord('a')] += 1
+            hmap[tuple(char_count)].append(word)
 
-
-        
+        return list(hmap.values())
