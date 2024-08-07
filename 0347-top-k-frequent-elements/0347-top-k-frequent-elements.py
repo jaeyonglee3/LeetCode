1b84a6aeae2088b3c3f87c03ea18544a7be5a5c3
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-06 22:54:36 -0400

    Time: 87 ms (67.65%), Space: 20.7 MB (94.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)  # {1 : 3, 2 : 2, 3 : 1}
         res = []
-        freq = dict(sorted(freq.items(), key=lambda item: item[1]))
-        freqs = freq.keys()
-        freqs = list(freqs)
+        freq_map = defaultdict(lambda: 0)
+
+        for num in nums:
+            freq_map[num] += 1
+
+        sorted_map = dict(sorted(freq_map.items(), key=lambda item: item[1]))
+        sorted_keys = list(sorted_map.keys())
 
         for _ in range(k):
-            res.append(freqs.pop())
-
-        return res
+            res.append(sorted_keys.pop())
         
+        return res
