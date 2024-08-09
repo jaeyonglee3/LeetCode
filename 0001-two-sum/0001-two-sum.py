fcedf37e5e85714bd43fc607c4eda96d5a259db4
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-08 23:48:44 -0400

    Time: 3287 ms (5.57%), Space: 17.4 MB (72.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # First, the O(n^2) solution
         for i, num1 in enumerate(nums):
             for j, num2 in enumerate(nums):
-                if (num1 + num2 == target and i != j):
+                if num1 + num2 == target and i != j:
                     return [i, j]
-        
