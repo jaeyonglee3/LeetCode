143815fcd94ecb4bc41ba6a56f30745c74a2c855
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-08 23:51:54 -0400

    Time: 58 ms (69.84%), Space: 17.7 MB (51.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # First, the O(n^2) solution
-        for i, num1 in enumerate(nums):
-            for j, num2 in enumerate(nums):
-                if num1 + num2 == target and i != j:
-                    return [i, j]
+        # for i, num1 in enumerate(nums):
+        #     for j, num2 in enumerate(nums):
+        #         if num1 + num2 == target and i != j:
+        #             return [i, j]
+
+        # Now, the O(n) with hashmap
+        hmap = {}
+
+        for i, num in enumerate(nums):
+            if target - num in hmap:
+                return [hmap[target - num], i]
+            
+            hmap[num] = i
