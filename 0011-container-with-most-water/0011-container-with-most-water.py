d3f62adbe5beacad583391ebf5a29a82313c8637
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-10 22:03:11 -0400

    Time: 475 ms (95.56%), Space: 29.8 MB (22.13%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,31 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # BRUTE FORCE TLE, O(n^2)
-        # max_area = 0
-        # for i in range(len(height)):
-        #     for j in range(i, len(height)):
-        #         h = min(height[i], height[j])
-        #         width = j - i
-        #         area = h * width
-        #         max_area = max(max_area, area)
-        
-        # return max_area
+        res = 0
+        l, r = 0, len(height) - 1
 
-        max_area = 0
-        l, r = 0, len(height) - 1  # initialize with max possible width
-        # calculate area each time
-        # increment/decrement the pointer with the shorter height
-        # stop when l < r is false
-        # O(n) runtime
-
-        while l < r:
-            area = (r - l) * min(height[l], height[r])
-            max_area = max(max_area, area)
-
-            if height[r] > height[l]:
-                l += 1
-            else:
+        while r > l:
+            smaller = min(height[l], height[r])
+            curr_area = smaller * (r - l)
+            if curr_area > res:
+                res = curr_area
+            
+            if height[r] == smaller:
                 r -= 1
+            else:
+                l += 1
         
-        return max_area
-
+        return res
