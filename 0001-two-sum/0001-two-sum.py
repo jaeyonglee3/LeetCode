f525c531fcf6f3fa1da95836a9e3499fc0461f65
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-19 22:34:39 -0400

    Time: 60 ms (60.36%), Space: 17.7 MB (54.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # First, O(n^2) solution
-        for i, num1 in enumerate(nums):
-            for j, num2 in enumerate(nums):
-                if num1 + num2 == target and i != j:
-                    return [i, j]
-                    
+        # for i, num1 in enumerate(nums):
+        #     for j, num2 in enumerate(nums):
+        #         if num1 + num2 == target and i != j:
+        #             return [i, j]
+
+        # O(n) solution
+        seen = {}
+
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [i, seen[target - num]]
+            else:
+                seen[num] = i
