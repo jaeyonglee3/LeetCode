5d440579c9f4f3344bd5df816b1f7c2a34ec9ff6
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-20 00:00:54 -0400

    Time: 55 ms (68.57%), Space: 16.7 MB (28%) - LeetHub

--- 1547-destination-city/1547-destination-city.py
+++ 1547-destination-city/1547-destination-city.py
@@ -1,12 +1,20 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        all_elements = set()
-        origin_elements = set()
+        # all_elements = set()
+        # origin_elements = set()
 
-        for path in paths:
-            all_elements.add(path[0])
-            all_elements.add(path[1])
-            origin_elements.add(path[0])
+        # for path in paths:
+        #     all_elements.add(path[0])
+        #     all_elements.add(path[1])
+        #     origin_elements.add(path[0])
         
-        dest = (all_elements - origin_elements).pop()
-        return dest
+        # dest = (all_elements - origin_elements).pop()
+        # return dest
+
+        map = dict()
+        for i in paths:
+            map[i[0]] = i[1]
+        for j in paths:
+            if j[1] not in map:
+                return j[1]
+        return "" 
