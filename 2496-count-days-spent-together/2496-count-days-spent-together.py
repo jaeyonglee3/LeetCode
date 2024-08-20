59aa8834e3ee555a1069f480deacd49746247b8f
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-19 21:56:06 -0400

    Time: 38 ms (34.58%), Space: 16.5 MB (82.08%) - LeetHub

--- 2496-count-days-spent-together/2496-count-days-spent-together.py
+++ 2496-count-days-spent-together/2496-count-days-spent-together.py
@@ -2,6 +2,7 @@ class Solution:
     def countDaysTogether(self, arriveAlice: str, leaveAlice: str, arriveBob: str, leaveBob: str) -> int:
         return max(0, self.getDayOfYear(min(leaveAlice, leaveBob)) - self.getDayOfYear(max(arriveAlice, arriveBob)) + 1)
     
+    # For example, "01-01" is day 1, and "12-31" is day 365
     def getDayOfYear(self, date):
         monthList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         month = int(date[:2])
