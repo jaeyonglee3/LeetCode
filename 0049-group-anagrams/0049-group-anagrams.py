d4bedffe996eee32483d47e6f1ffcb85783c0a8c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-08-20 19:54:06 -0400

    Time: 80 ms (92.35%), Space: 19.4 MB (94.15%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hmap = defaultdict(list)
+        # if two words are anagrams, they'll be the SAME word when their letters are put alphabetically
+        # nat, tan -> ant
+        # Use a hashmap where key = alphabetically organized word. Values are arrays of words from strs
+
+        words = {}
 
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            hmap[sorted_word].append(word)
+            sorted_word = "".join(sorted(word))
+            if sorted_word in words:
+                words[sorted_word].append(word)
+            else:
+                words[sorted_word] = [word]
         
-        return list(hmap.values())
-        
+        # To return, must compile a list of all the values from words
+        result = list(words.values())
+        return result        
