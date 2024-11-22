0d78fc3edefd09388b60ccd56cd40e19539a5fe1
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-11-22 00:14:18 -0500

    Time: 123 ms (27.92%), Space: 25.5 MB (99.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Sliding window approach
-        # Day of buying stock < Day of selling the stock
-        # Profit = Sell Price - Buy Price
-        l, r = 0, 1
         max_profit = 0
+        l, r = 0, 1
 
-        while r < len(prices):
+        while r < len(prices) and r > l:
             curr_profit = prices[r] - prices[l]
-            max_profit = max(curr_profit, max_profit)
-            
+            max_profit = max(max_profit, curr_profit)
+
             if curr_profit < 0:
                 l = r
             r += 1
         
         return max_profit
-
