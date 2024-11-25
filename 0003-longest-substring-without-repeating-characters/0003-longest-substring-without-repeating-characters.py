2030965eff069616fc21b91ac68cc6be369e618d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-11-24 23:12:58 -0500

    Time: 7 ms (99.41%), Space: 16.7 MB (72.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,9 +7,13 @@ class Solution:
         for r in range(len(s)):
             char = s[r]
 
+            # If the current character (char) is already in visited and 
+            # its last seen index is within the current window,
+            # a duplicate has been found
             if char in visited and visited[char] >= l:
-                l = visited[char] + 1
+                l = visited[char] + 1 # shrink window to leave out duplicate
             else:
+                # no duplicate found, curr window contains a valid substring
                 res = max(res, r + 1 - l)
             
             visited[char] = r
