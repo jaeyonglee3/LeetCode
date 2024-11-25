ed019c6fad5063d760052c37e96723cc7a623489
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-11-24 22:17:48 -0500

    Time: 3 ms (65.01%), Space: 18.4 MB (21.4%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -6,7 +6,7 @@ class Solution:
             "+": operator.add,
             "-": operator.sub,
             "*": operator.mul,
-            "/": operator.truediv, # floordiv b/c division always truncates towards zero
+            "/": operator.truediv, # floordiv doesn't work because it'd round -0.4 to -1 instead of 0
         }
         stack = []
         
@@ -16,7 +16,8 @@ class Solution:
                 ls = stack.pop()
                 operation = operators[token]
                 new_val = operation(ls, rs)
-                stack.append(int(new_val))
+                # stack.append(int(new_val))
+                stack.append(int(math.trunc(new_val)))
             else:
                 stack.append(int(token))
         
