3ab5ad2af7bf8cd79d3bcb82f37f191d73c4ba2c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-11-26 22:52:16 -0500

    Time: 1111 ms (16.53%), Space: 21.3 MB (8.11%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -32,11 +32,6 @@ class Solution:
                     if triple not in seen:
                         res.append(list(triple))
                         seen.add(triple)
-                    # Optimization: Skip duplicate numbers for 'l' and 'r'
-                    while l < r and nums[l] == nums[l + 1]:  
-                        l += 1
-                    while l < r and nums[r] == nums[r - 1]:
-                        r -= 1
                     l += 1
                     r -= 1
         
