da74115258089a30871bec69e48f76dc9028a32f
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-11-26 23:01:47 -0500

    Time: 547 ms (72.65%), Space: 20.1 MB (44.59%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -33,8 +33,12 @@ class Solution:
                 else:
                     res.append([nums[l], nums[r], x])
                     # e.g. [-2, -2, 0, 0, 2, 2]
-                    l += 1
-                    while nums[l] == nums[l - 1] and r > l:
+                    # Optimization: Skip duplicate numbers for 'l' and 'r'
+                    while l < r and nums[l] == nums[l + 1]:  
                         l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
         
         return res
