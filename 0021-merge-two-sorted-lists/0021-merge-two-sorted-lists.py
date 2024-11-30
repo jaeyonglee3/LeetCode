9bd0bfbbcf91a284d1778d3f61bcdfd94c6e2187
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-11-30 01:13:34 -0500

    Time: 0 ms (100%), Space: 17.2 MB (15.22%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,24 +6,22 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         curr1, curr2 = list1, list2
-
-        # fakenode will always point to head while result moves around
-        # so the two only point to the same node initially
-        res = fakenode = ListNode()
+        # a node with default value serves as the head of the result
+        res_head = res_curr = ListNode()
 
         while curr1 and curr2:
             if curr1.val < curr2.val:
-                res.next = curr1
+                res_curr.next = curr1
                 curr1 = curr1.next
             else:
-                res.next = curr2
+                res_curr.next = curr2
                 curr2 = curr2.next
             
-            res = res.next
+            res_curr = res_curr.next
         
         if curr1:
-            res.next = curr1
+            res_curr.next = curr1
         elif curr2:
-            res.next = curr2
+            res_curr.next = curr2
         
-        return fakenode.next
+        return res_head.next
