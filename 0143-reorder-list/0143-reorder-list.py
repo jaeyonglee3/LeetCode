437a2d334d66930d59dac3145839dc6eecfc6871
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-01 00:19:15 -0500

    Time: 6463 ms (5.42%), Space: 22.8 MB (32.99%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,53 +8,22 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # Approach 1: Store linkedlist in an array (uses extra space)
-        # llist = []
-
-        # curr = head
-        # while curr:
-        #     llist.append(curr)
-        #     curr = curr.next
-        
-        # l, r = 0, len(llist) - 1
-
-        # while l < r:
-        #     llist[l].next = llist[r]
-        #     l += 1
-        #     llist[r].next = llist[l]
-        #     r -= 1
-        
-        # llist[l].next = None
-
-        #------------------------------------------------
-        # Approach 2: (doesn't use any extra memory)
         slow, fast = head, head.next
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        # Now reverse linkedlist from slow to fast window
-        # Increment slow once more to include in portion to reverse
-        slow = slow.next
-        
-        prev, curr = None, slow
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev, curr = curr, nxt
-        
-        # At this point, the 2nd half of the linkedlist has been reversed
-        # fast may be None or the last node of the origin LL
-        
-        left, right = head, prev
-        while right and left:
-            left_temp = left.next
-            left.next = right
-            left = left_temp
+        while slow:
+            prev = None
 
-            right_temp = right.next
-            right.next = left
-            right = right_temp
-        
-        left.next = None
+            while fast and fast.next:
+                prev = fast
+                fast = fast.next
+
+            if prev:
+                old_slow_next = slow.next
+                slow.next = fast
+                
+                slow = old_slow_next
+                fast.next = slow
+                prev.next = None
+                fast = slow.next
+            else:
+                return
