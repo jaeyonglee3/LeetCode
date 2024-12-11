f9891a70570f3d910dff01956e4dd6e75c60d66c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-10 23:47:15 -0500

    Time: 0 ms (100%), Space: 17.2 MB (10.78%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -6,7 +6,9 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Pre order traversal means visiting the root node first before its descendants
+        # Visiting order: current, left, then finally right subtrees
         if not root:
             return []
-
+        
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
