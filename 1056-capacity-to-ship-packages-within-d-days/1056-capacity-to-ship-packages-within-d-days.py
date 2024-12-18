b8654ac2aeafcfbfd8821fd460fb239490390c26
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-17 22:07:30 -0500

    Time: 454 ms (5.02%), Space: 22.3 MB (8.88%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -5,21 +5,14 @@ class Solution:
             curr_package = 0
             days_taken = 0
 
-            print("CAPACITY: ", capacity)
             while curr_package < len(weights):
                 while curr_package < len(weights) and curr_load + weights[curr_package] <= capacity:
-                    # print(curr_load)
-                    # add the weight of the current package to current load
-                    print(weights[curr_package])
                     curr_load += weights[curr_package]
                     curr_package += 1
-                    # print(curr_package)
                 
-                # print("")
                 curr_load = 0
                 days_taken += 1
             
-            # print("days taken: ", days_taken)
             return days_taken <= days
         
         l, r = max(weights), sum(weights)
