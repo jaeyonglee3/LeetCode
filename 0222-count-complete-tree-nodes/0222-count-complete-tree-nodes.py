8c5dbe8078403d22d29383c9e633ddd81a35046b
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-17 20:48:18 -0500

    Time: 0 ms (100%), Space: 23.5 MB (8.24%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -13,15 +13,18 @@ class Solution:
         r_height = self.findHeight(root.right)
         
         if l_height == r_height:
-            # It is guaranteed that left is a perfect BT
+            # It is guaranteed that left subtree is a perfect BT
             # Perfect BT has 2^(height) - 1 nodes
             return int(math.pow(2, l_height)) + self.countNodes(root.right)
         else:
+            # It is guaranteed that right subtree is a perfect BT
+            # Perfect BT has 2^(height) - 1 nodes
             return int(math.pow(2, r_height)) + self.countNodes(root.left)
         
     
     def findHeight(self, root) -> int:
-        if not root:
-            return 0
-        
-        return 1 + max(self.findHeight(root.left), self.findHeight(root.right))
+        height = 0
+        while root:
+            height += 1
+            root = root.left  # Only traverse the left subtree
+        return height
