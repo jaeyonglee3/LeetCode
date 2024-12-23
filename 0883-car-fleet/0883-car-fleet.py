71273328df2122aff0940b4d500d87918abc2af9
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-23 13:06:12 -0500

    Time: 162 ms (52.57%), Space: 36.7 MB (81.13%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,32 +1,26 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # Key Idea: sort cars by position and iterate in reversed order
-        # At each car, calculate time at which destination is reached
-        # Compare with lead_car_time. If lead_car_time is smaller, that means they'll form a fleet 
-        # because dest_time is greater than lead_car_time
-        # res += 1 and curr_time = calculated des time
+        # Hold all fleets in a stack (nested array), each element is a fleet
+        # Iterate array in reverse. Calculate arrival time of each car (target-position)/speed
+        # If a car that is behind a car arrives sooner or at the same time, 
+        # the two must overlap and form a fleet at some point
+        # pop off the one travelling at a faster speed
+        # (fleets travel at the speed of the slowest car)
 
-        # cars = list(zip(position, speed))
-        # res = lead_car_time = 0 # lead_car_time is time taken by leading car of curr fleet
-        # # in reverse bc cars closer to target have potential to form fleets w cars behind them
-        # for dist, speed in sorted(cars, reverse=True): 
-        #     destination_hour = (target - dist)/speed
-        #     if lead_car_time < destination_hour:
-        #         res += 1
-        #         lead_car_time = destination_hour
-        
-        # return res
-
-        # NEETCODE - stack solution
+        fleets = []
         cars = list(zip(position, speed))
-        stack = []
-        for dist, speed in sorted(cars, reverse=True):
-            stack.append((target - dist)/speed)
-            if len(stack) > 1 and stack[-1] <= stack[-2]:
-                # always popping cars in the fleet with faster speeds 
-                # so that only the slowest cars remain
-                # and stack eventually only contains 1 car from each fleet.
-                stack.pop() 
-        
-        return len(stack)
 
+        for pos, speed in sorted(cars, reverse=True):
+            fleets.append((target-pos)/speed)
+
+            # Check if car just added arrives sooner or 
+            # at same time than the car ahead of it
+            # but only check if >= 2 fleets currently
+            if len(fleets) >= 2 and fleets[-1] <= fleets[-2]:
+                fleets.pop()
+        
+        return len(fleets)
+
+
+
+        
