42420fad1857bdd08121f5772e3faaf45a2c3bf5
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-23 13:29:30 -0500

    Time: 647 ms (34.81%), Space: 20.5 MB (20.78%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,37 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # P - return an array of arrays each containing 3 nums that add up to 0 w no duplicates
-        # C - 
-        # C - how to iterate nums? 
-        # L -
-        # I - 
-        # M -
-        # T -
-
-        nums.sort()
         res = []
+        nums.sort()
 
-        for x_i, x in enumerate(nums):
+        for i, i_val in enumerate(nums):
+            l, r = i + 1, len(nums) - 1
             # Optimization: Skip duplicate numbers for the fixed 'x'
             # if it isn't the first value in nums
             # and if its the same num as the one before it, skip it!
-            if x_i > 0 and nums[x_i] == nums[x_i - 1]:  
+            if i > 0 and i_val == nums[i - 1]:  
                 continue 
 
-            target = -x
-            # l starts at x_i + 1 because we've already considered all 
-            # possible triplets with x as the fixed value and indices less than x_i in previous iterations.
-            l, r = x_i + 1, len(nums) - 1
-            
             while r > l:
                 curr_sum = nums[l] + nums[r]
-
-                if curr_sum < target:
-                    l += 1
-                elif curr_sum > target:
-                    r -= 1
-                else:
-                    res.append([nums[l], nums[r], x])
+                
+                if curr_sum == -i_val:
+                    res.append([i_val, nums[r], nums[l]])
                     # e.g. [0, -2, -2, 0, 0, 2, 2]
                     # Optimization: Skip duplicate numbers for 'l' and 'r'
                     # Because its sorted, all duplicates would be next to each other!
@@ -41,5 +25,14 @@ class Solution:
                         r -= 1
                     l += 1
                     r -= 1
+                elif curr_sum > -i_val:
+                    r -= 1
+                else:
+                    l += 1
         
         return res
+
+
+
+
+
