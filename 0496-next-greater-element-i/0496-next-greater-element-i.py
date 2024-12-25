5c8cdc768967f42ee7856778f905513821009bef
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-25 18:00:19 -0500

    Time: 0 ms (100%), Space: 18.1 MB (6.78%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,26 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # monotonically decreasing stack problem, solves in O(n1 * n2) time
-
+        # monotonically decreasing stack problem, solves in O(n1 + n2) time
         res = [-1] * len(nums1)
         stack = []
-        nums1_map = {val : i for i, val in enumerate(nums1)}
-        # iterate through nums2 and keep a stack of nums
-        # before adding to the stack, check if num being added
-        # is greater. If so, we've found stack[-1]'s next greater num.
-        # if stack[-1] is in nums1, then res at the same index turns to the difference
-        # in the indecies between the curr num2 and stack[-1] in nums2
+        next_greater = {}
 
         for i, num2 in enumerate(nums2):
-            # if its greater than value at top of the stack
-            # start popping from stack
             while stack and num2 > stack[-1]:
                 removed = stack.pop()
-                # make res at index of removed element in nums1 equal num2
-                # iff nums1 contains the just removed element
-                if removed in nums1:
-                    res[nums1_map[removed]] = num2
+                next_greater[removed] = num2
             
             stack.append(num2)
         
-        return res
+        return [next_greater.get(num, -1) for num in nums1]
