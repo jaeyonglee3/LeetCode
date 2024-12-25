18e0e2b99a49322396319f83196d31575e5c0d20
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-25 17:42:56 -0500

    Time: 16 ms (23.15%), Space: 17.9 MB (14.76%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,7 +3,8 @@ class Solution:
         # monotonically decreasing stack problem
 
         res = [-1] * len(nums1)
-        nums = []
+        stack = []
+        nums1_map = {val : i for i, val in enumerate(nums1)}
         # iterate through nums2 and keep a stack of nums
         # before adding to the stack, check if num being added
         # is greater. If so, we've found stack[-1]'s next greater num.
@@ -13,13 +14,13 @@ class Solution:
         for i, num2 in enumerate(nums2):
             # if its greater than value at top of the stack
             # start popping from stack
-            while nums and num2 > nums[-1]:
-                removed = nums.pop()
+            while stack and num2 > stack[-1]:
+                removed = stack.pop()
                 # make res at index of removed element in nums1 equal num2
                 # iff nums1 contains the just removed element
                 if removed in nums1:
-                    res[nums1.index(removed)] = num2
+                    res[nums1_map[removed]] = num2
             
-            nums.append(num2)
+            stack.append(num2)
         
         return res
