1440e1bb65d982b5731022fed555aac7f066b328
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-25 17:54:48 -0500

    Time: 0 ms (100%), Space: 18 MB (10.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,26 +1,24 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # monotonically decreasing stack problem, solves in O(n1 * n2) time
-
-        res = [-1] * len(nums1)
         stack = []
-        nums1_map = {val : i for i, val in enumerate(nums1)}
-        # iterate through nums2 and keep a stack of nums
-        # before adding to the stack, check if num being added
-        # is greater. If so, we've found stack[-1]'s next greater num.
-        # if stack[-1] is in nums1, then res at the same index turns to the difference
-        # in the indecies between the curr num2 and stack[-1] in nums2
+        hashmap = {}
+        output = []
 
-        for i, num2 in enumerate(nums2):
-            # if its greater than value at top of the stack
-            # start popping from stack
-            while stack and num2 > stack[-1]:
-                removed = stack.pop()
-                # make res at index of removed element in nums1 equal num2
-                # iff nums1 contains the just removed element
-                if removed in nums1:
-                    res[nums1_map[removed]] = num2
+        for i in reversed(nums2):
+            while stack:
+                #found nearest greater element
+                if stack[-1] > i:
+                    hashmap[i] = stack[-1]
+                    stack.append(i)
+                    break
+                else: 
+                    stack.pop()
             
-            stack.append(num2)
+            #stack is empty initially or was popped till empty
+            if not stack:
+                hashmap[i] = -1
+                stack.append(i)
         
-        return res
+        for j in nums1:
+            output.append(hashmap[j])
+        return output
