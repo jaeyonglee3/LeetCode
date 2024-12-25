4bd64a020ac0218a91bb46ca45ea6ffb30ca0b62
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-25 16:32:37 -0500

    Time: 132 ms (26.49%), Space: 33.1 MB (23.32%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,67 +1,20 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # P - return an array ans where ans[i] represents the number of days before a warmer temp than temp on i-th day
-        # C - 
-        # L -
-        # I - use stack. push until you reach temp that is higher than starting temp. Pop until you reach starting temp. Count num pops. add count to answers arr
-        # M -
-        # T -
+        # need a stack that is montonically decreasing
 
-        # Naive solution with for loop and nested while loop, TLE
-        # answer = []
-        # for i, temp in enumerate(temperatures):
-        #     if i != len(temperatures) - 1:
-        #         day_count = 1
-        #         next_temp = i + 1
-            
-        #         while temperatures[next_temp] <= temp and next_temp < len(temperatures) - 1:
-        #             next_temp += 1
-        #             day_count += 1
-                
-        #         if temperatures[next_temp] > temp:
-        #             answer.append(day_count)
-        #         else:
-        #             answer.append(0)
+        res = len(temperatures) * [0]
+        t_stack = [] # (index, temp)
 
-        #     else:
-        #         answer.append(0)
-    
-        # return answer
-
-        # TLE
-        # answer = []
-        # t_stack = []
-        # for i, temp in enumerate(temperatures):
-        #     if (i == len(temperatures) - 1):
-        #         answer.append(0)
-        #     else:
-        #         t_stack.append(temp)
-        #         next_temp = i + 1
-
-        #         # push until youve pushed smth greater
-        #         while next_temp < len(temperatures) and temperatures[next_temp] <= temp:
-        #             t_stack.append(temperatures[next_temp])
-        #             next_temp += 1
-                
-        #         if next_temp > len(temperatures) - 1:
-        #             answer.append(0)
-        #         else:
-        #             # pop until youre at temp again
-        #             while t_stack[-1] != temp:
-        #                 t_stack.pop()
-                    
-        #             answer.append(next_temp - i) # Calculate num_popped directly
-                    
-        # return answer
-        
-        res = [0] * len(temperatures)
-        t_stack = [] # Pair: (temp, index)
-
+        # iterate over the temperatures array
         for i, temp in enumerate(temperatures):
-            while t_stack and temp > t_stack[-1][0]:
-                removed = t_stack.pop()
-                res[removed[1]] = i - removed[1]
+            # check if the current temperature is greater
+            # than the temp sitting at the top of the stack.
+            # if true, we can update the answer in the results arr
+            # for the temp for which we've just found a bigger one
+            while t_stack and temp > t_stack[-1][1]:
+                res[t_stack[-1][0]] = i - t_stack[-1][0]
+                t_stack.pop()
             
-            t_stack.append((temp, i))
+            t_stack.append((i, temp))
         
         return res
