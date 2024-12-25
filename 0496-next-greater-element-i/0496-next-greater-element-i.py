9b22d1fbbe228ba8ac12dc78297bc5f0182e4e16
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-25 18:05:24 -0500

    Time: 0 ms (100%), Space: 17.9 MB (10.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,13 +3,15 @@ class Solution:
         # monotonically decreasing stack problem, solves in O(n1 + n2) time
         res = [-1] * len(nums1)
         stack = []
-        next_greater = {}
+        nums1_map = {val : i for i, val in enumerate(nums1)}
 
         for i, num2 in enumerate(nums2):
             while stack and num2 > stack[-1]:
                 removed = stack.pop()
-                next_greater[removed] = num2
+
+                if removed in nums1_map:
+                    res[nums1_map[removed]] = num2
             
             stack.append(num2)
         
-        return [next_greater.get(num, -1) for num in nums1]
+        return res
