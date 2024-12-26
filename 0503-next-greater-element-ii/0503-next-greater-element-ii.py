8edf15b19c2a6b24e1072b9e7b2788a01b73afee
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-25 19:47:40 -0500

    Time: 26 ms (51.12%), Space: 19.5 MB (12.86%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,19 +1,15 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        # iterate through nums twice b/c its circular
-
         n = len(nums)
         res = [-1] * n
         stack = []
 
-        for i in range(n * 2):
-            while stack and nums[i % n] > nums[stack[-1] % n]:
+        # iterate through nums twice because it is circular
+        for i in range(2 * n):
+            while stack and nums[i % n] > nums[stack[-1]]:
                 removed = stack.pop()
                 res[removed] = nums[i % n]
             
-
-            # For the first pass, we need to fill the stack with index.
-            # Avoid pushing index onto the stack in the second pass to prevent duplicating work.
             if i < n:
                 stack.append(i % n)
         
