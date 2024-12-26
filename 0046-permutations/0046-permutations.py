72db71accbb5d3bdecc8c9f74365a9a62ecf6edc
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-26 15:50:33 -0500

    Time: 0 ms (100%), Space: 18 MB (6.56%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(i):
+            if i == len(nums):
+                res.append(curr.copy())
+                return
+            
+            for j in range(len(nums)):
+                if not visited[j]:
+                    curr[i] = nums[j]
+                    visited[j] = True
+                    dfs(i + 1)
+                    visited[j] = False
+        
+        len_nums = len(nums)  # Store the length of the input list
+        visited = [False] * len_nums  # Create a visited list to track numbers that are used
+        curr = [0] * len_nums  # Temp list to store the current permutation
+        
+        dfs(0)  # Start generating permutations from index 0
+        return res
