6509ca6bcf9a1bdbe19796236c8a82d187455cd3
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-28 00:09:08 -0500

    Time: 19 ms (18.22%), Space: 18.7 MB (6.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-        count_s = {}
-
-        for c in s:
-            count_s[c] = 1 + count_s.get(c, 0)
-        
-        for c in set(t):
-            if t.count(c) != count_s.get(c, 0):
-                return False
-
-        return True
-
-        # first, populate a hashmap with letters to how many times they're used in s
-        # iterate through the chars in t, using count function and comparing to the number in hashmap
-        # if numbers aren't same, return false
+        return sorted(s) == sorted(t)
