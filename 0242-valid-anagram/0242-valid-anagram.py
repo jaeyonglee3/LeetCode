078bd743a253b8c167009b262a45ba5c29485168
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-28 00:11:08 -0500

    Time: 11 ms (75.18%), Space: 17.8 MB (18.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        freq_map_t = {}
+        freq_map_s = {}
+
+        for char in t:
+            if char in freq_map_t:
+                freq_map_t[char] += 1
+            else:
+                freq_map_t[char] = 1
+        
+
+        for char in s:
+            if char in freq_map_s:
+                freq_map_s[char] += 1
+            else:
+                freq_map_s[char] = 1
+        
+        return freq_map_t == freq_map_s
