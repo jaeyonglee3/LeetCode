eeeb0342c48dd658c76b8df1021fc47266cf1a9f
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-30 14:57:58 -0500

    Time: 0 ms (100%), Space: 17.6 MB (12.26%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -6,16 +6,16 @@ class Solution:
         for char in s:
             if char == "]":
                 # 1. Form the string that will be repeated
-                # 2. Form the number so we know how many times to repeat the string
-
                 new_str = ""
                 while stack[-1] != "[":
                     new_str = stack.pop() + new_str
                 
-                stack.pop() # removed the opening bracket
+                # 2. Remove the opening bracket
+                stack.pop()
 
+                # 3. Form the number so we know how many times to repeat the string
                 num_repeat = ""
-                while stack and stack[-1].isnumeric():
+                while stack and stack[-1].isdigit():
                     num_repeat = stack.pop() + num_repeat
 
                 stack.append(new_str * int(num_repeat))
@@ -23,4 +23,5 @@ class Solution:
             else:
                 stack.append(char)
         
+        # By the end, the stack contains the entire decoded string
         return "".join(stack)
