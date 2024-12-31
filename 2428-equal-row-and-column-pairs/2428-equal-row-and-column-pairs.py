4c5a0eef5c991016a4f9d17896e582c147d21242
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2024-12-31 14:47:26 -0500

    Time: 12 ms (90.22%), Space: 21.9 MB (34.97%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -1,15 +1,6 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        # Use a nested loop to compare every row to every column
-        # cols = defaultdict(tuple)
-        # rows = defaultdict(tuple)
-
-        # for row_num, row in enumerate(grid):
-        #     for col_num, val in enumerate(row):
-        #         rows[row_num].append(val)
-        #         cols[col_num].append(val)
-        
-        # Now, determine the number of equal row and column pairs
+        # Convert rows to tuple and count their frequencies
         row_count = Counter(tuple(row) for row in grid)
 
         # Count matches between columns and rows
@@ -18,11 +9,3 @@ class Solution:
             res += row_count[tuple(col)]
         
         return res
-
-        # all_rows = list(rows.values())
-        # res = 0
-
-        # for col in cols.values():
-        #     res += all_rows.count(col)
-
-        # return res
