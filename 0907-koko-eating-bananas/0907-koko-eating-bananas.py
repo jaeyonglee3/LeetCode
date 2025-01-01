67ae9bddf132c46b121882f37bf7deb437cc472a
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-01 17:38:05 -0500

    Time: 173 ms (28.28%), Space: 19.2 MB (7.53%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,22 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # isFeasible helper fn returns True if all bananas can be eaten
-        # within h hours based on speed k
-        def isFeasible(k) -> bool:
-            total_hours = 0
-            for pile in piles:
-                if pile < k:
-                    total_hours += 1
-                else:
-                    total_hours += math.ceil(pile/k)
+        # search window for k: 1 to max(piles)
+        # find the slowest values for k such that all can be eaten within h hours
 
-            return total_hours <= h
+        def isFeasible(k):
+            total_hrs = 0
+            for pile in piles:
+                if k >= pile:
+                    total_hrs += 1
+                else:
+                    total_hrs += math.ceil(pile/k)
+            
+            return total_hrs <= h
         
-        res = max_k = max(piles)
-        l, r = 1, max_k - 1
+        l, r = 1, max(piles)
+        res = 0
 
         while r >= l:
             mid = (l + r) // 2
+            print(mid)
 
             if isFeasible(mid):
                 res = mid
