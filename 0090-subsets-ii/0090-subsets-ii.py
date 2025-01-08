1462ae58900e6a6ecade51acab34061ab8e739fb
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-07 23:30:11 -0500

    Time: 0 ms (100%), Space: 18.2 MB (6.04%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -4,21 +4,23 @@ class Solution:
         nums.sort()
 
         subset = []
-        def dfs(i):
+        def dfs(i, subset):
             if i == len(nums):
                 res.append(subset[:])
                 return
             
+            # All subsets that include nums[i]
             # Left branch, add nums[i]
             subset.append(nums[i])
-            dfs(i + 1)
+            dfs(i + 1, subset)
 
+            # All subsets that don't include nums[i]
             # Right branch, do not include nums[i]
             # but, be sure to skip any duplicates
             subset.pop()
             while i < len(nums) - 1 and nums[i] == nums[i + 1]:
                 i += 1
-            dfs(i + 1)
+            dfs(i + 1, subset)
         
-        dfs(0)
+        dfs(0, subset)
         return res
