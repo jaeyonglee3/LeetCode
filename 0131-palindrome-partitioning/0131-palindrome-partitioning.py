19532269b4049b72ebffa9563b35461df5f20636
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-15 20:40:58 -0500

    Time: 51 ms (55.66%), Space: 34.5 MB (30.11%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -4,25 +4,27 @@ class Solution:
         partition = []
 
         def backtrack(i):
-            if i >= len(s):
+            if i > len(s) - 1:
                 res.append(partition[:])
                 return
             
             for j in range(i, len(s)):
-                curr_string = s[i : j + 1]
-                if self.isPalindrome(curr_string):
-                    partition.append(curr_string)
+                curr_sstring = s[i : j + 1]
+                if self.isPalindrome(curr_sstring):
+                    partition.append(curr_sstring)
                     backtrack(j + 1)
                     partition.pop()
         
         backtrack(0)
         return res
+        
+    def isPalindrome(self, word):
+        l, r = 0, len(word) - 1
 
-    def isPalindrome(self, s):
-        l, r = 0, len(s) - 1
         while r > l:
-            if s[l] != s[r]:
+            if word[l] != word[r]:
                 return False
+            
             l += 1
             r -= 1
         
