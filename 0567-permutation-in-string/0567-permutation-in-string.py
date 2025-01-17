18edccb81ec95d63997badcdb2b7dadeec2ae127
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-16 23:59:55 -0500

    Time: 6091 ms (5%), Space: 17.9 MB (33.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,14 +1,14 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # Fixed sliding window approach
-        size = len(s1)
-        left = 0
+        # fixed sliding window approach
+        left, right = 0, len(s1)
 
-        while left + size <= len(s2):
-            sstring = s2[left: left + size]
-            if sorted(sstring) == sorted(s1):
+        while right <= len(s2):
+            substring = s2[left : right]
+            if sorted(substring) == sorted(s1):
                 return True
             
             left += 1
-            
+            right = left + len(s1)
+        
         return False
