1b65940e3ef3779067155836165488fbd2aa2805
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-18 17:03:12 -0500

    Time: 9 ms (14.55%), Space: 17.8 MB (41.68%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,26 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)
-        valid_numbers = set("123456789")
+        # For each 3x3 box, we'll use the top left corner value as a unique identifier to store
+        # the values belonging to the subbox
+        boxes_map = collections.defaultdict(set)
+        rows_map = collections.defaultdict(set)
+        cols_map = collections.defaultdict(set)
+        valid_nums = set("123456789")
 
         for row_num, row in enumerate(board):
             for col_num, val in enumerate(row):
-                if val in valid_numbers:
-                    if (val in rows[row_num] or val in cols[col_num] or val in squares[(row_num // 3, col_num // 3)]):
-                            return False
+                if val in valid_nums:
+                    # before adding to the maps, create copies of them to see if it grows or not
+                    row_copy = rows_map[row_num].copy()
+                    col_copy = cols_map[col_num].copy()
+                    boxes_map_copy = boxes_map[(row_num // 3, col_num // 3)].copy()
+                    
+                    # add it to the maps and continue
+                    rows_map[row_num].add(val)
+                    cols_map[col_num].add(val)
+                    boxes_map[(row_num // 3, col_num // 3)].add(val)
 
-                    # If no duplicate, add val to the sets
-                    rows[row_num].add(val)
-                    cols[col_num].add(val)
-                    squares[(row_num // 3, col_num // 3)].add(val)
-
-        return True  # If no duplicates were found, the board is valid
+                    if (rows_map[row_num] == row_copy or cols_map[col_num] == col_copy or boxes_map[(row_num // 3, col_num // 3)] == boxes_map_copy):
+                        return False
+        
+        return True
