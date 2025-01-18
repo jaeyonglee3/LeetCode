a540cf4ec91caf6fbfb72b4c5e8df204830aa1c1
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-18 18:21:24 -0500

    Time: 7 ms (99.36%), Space: 17.8 MB (30.41%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,21 +1,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s:
-            return 0
+        visited = {}
+        l = 0
+        res = 0
 
-        l = 0  # Left pointer
-        char_set = set()  # To track unique characters in the current window
-        res = 0  # Store the maximum length of substring
+        for r in range(len(s)):
+            char = s[r]
 
-        for r in range(len(s)):  # Right pointer iterates over the string
-            # If the character is already in the set, shrink the window from the left
-            while s[r] in char_set:
-                char_set.remove(s[l])
-                l += 1
-
-            # Add the current character to the set
-            char_set.add(s[r])
-            # Update the result with the current window size
-            res = max(res, r - l + 1)
-
+            # If the current character (char) is already in visited and 
+            # its last seen index is within the current window,
+            # a duplicate has been found
+            if char in visited and visited[char] >= l:
+                # shrink window by moving left to just one after the last place
+                # we saw it. this leaves out only the duplicate
+                l = visited[char] + 1
+            else:
+                # no duplicates, so the current window contains a valid substring
+                res = max(res, r - l + 1)
+            
+            visited[char] = r
+        
         return res
