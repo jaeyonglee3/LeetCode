8d326b8aa6d68ff10ad8c42ef34cb63ce39be493
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-19 18:30:07 -0500

    Time: 3 ms (83.32%), Space: 17.8 MB (42.81%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,13 +1,13 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         # Can solve using BFS
-        # Initial setup - count # of fresh oranges and add all
-        # rotten oranges to a queue
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         q = collections.deque() # contains only rotten oranges
         fresh = 0
         minutes = 0
-
+        
+        # Initial setup - count # of fresh oranges and add all
+        # rotten oranges to a queue
         for row_num, row in enumerate(grid):
             for col_num, val in enumerate(row):
                 if val == 1:
@@ -17,14 +17,15 @@ class Solution:
 
         # Ready now to begin BFS traversal
         while q and fresh > 0:
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 r, c = q.popleft()
                 
                 for dr, dc in directions:  
                     row, col = r + dr, c + dc
 
+                    # Check the bounds of the row and column
                     if row >= 0 and col >= 0 and row < len(grid) and col < len(grid[0]) and grid[row][col] == 1 :
-                        grid[row][col] = 2
+                        grid[row][col] = 2 # set that fresh orange to be a rotton one
                         fresh -= 1
                         q.append((row, col))
             
