50c27861eabb91cc97812997d16358ad8a1e3f15
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-19 18:21:05 -0500

    Time: 3 ms (83.32%), Space: 17.9 MB (16.03%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,36 +1,33 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        # Can solve using BFS
+        # Initial setup - count # of fresh oranges and add all
+        # rotten oranges to a queue
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        q = collections.deque()
+        q = collections.deque() # contains only rotten oranges
         fresh = 0
         minutes = 0
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1:
-                    # Count number of fresh oranges
+        for row_num, row in enumerate(grid):
+            for col_num, val in enumerate(row):
+                if val == 1:
                     fresh += 1
-                if grid[r][c] == 2:
-                    # Add all rotten oranges to queue
-                    q.append((r, c))
-        
-        while fresh > 0 and q:
+                if val == 2:
+                    q.append((row_num, col_num))
+
+        # Ready now to begin BFS traversal
+        while q and fresh > 0:
             for i in range(len(q)):
                 r, c = q.popleft()
-
-                for dr, dc in directions:
+                
+                for dr, dc in directions:  
                     row, col = r + dr, c + dc
-                    if (row in range(len(grid))
-                        and col in range(len(grid[0]))
-                        and grid[row][col] == 1
-                    ):
+
+                    if row >= 0 and col >= 0 and row < len(grid) and col < len(grid[0]) and grid[row][col] == 1 :
                         grid[row][col] = 2
-                        q.append((row, col))
                         fresh -= 1
+                        q.append((row, col))
             
             minutes += 1
-
-        if fresh > 0:
-            return -1
         
-        return minutes
+        return minutes if fresh == 0 else -1
