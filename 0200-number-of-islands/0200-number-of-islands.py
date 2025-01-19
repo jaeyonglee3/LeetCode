2a8af7de1148a29df5a3ffd3c92151d2f8e2b5cb
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-18 20:29:14 -0500

    Time: 245 ms (62.11%), Space: 20.3 MB (49.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         res = 0
 
         def dfs(r, c):
-            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == '0':
+            # BASE CASE
+            # r, c numbers are out of bounds OR grid[r][c] == "0"
+            if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == "0":
                 return
             
-            grid[r][c] = '0'
+            # Mark the current cell as 0 so its not visited again
+            grid[r][c] = "0"
+            
+            # Check every direction (up, down, left, right) from the current r, c position
             for direction in directions:
                 dfs(r + direction[0], c + direction[1])
-        
+
         for row_num, row in enumerate(grid):
             for col_num, val in enumerate(row):
                 if val == "1":
-                    res += 1
+                    # DFS on that island to mark all its connected 1s
+                    # so that connected 1s aren't mistakenly counted as separate islands!
                     dfs(row_num, col_num)
+                    res += 1
         
         return res
