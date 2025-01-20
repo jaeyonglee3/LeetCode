58e942773ae60ed040438f640bda1b682a2fed53
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-01-19 23:25:31 -0500

    Time: 11 ms (77.34%), Space: 18.3 MB (33.22%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        # -ve diagonals use r - c, +ve diagonals use r + c
+        cols, pos_diagonals, neg_diagonals = set(), set(), set()
+
+        res = []
+        board = [["."] * n for _ in range(n)] # initialize an empty n x n board filled with dots
+
+        # We'll go row by row starting from 0
+        def backtrack(r):
+            if r == n:
+                # Then we've found a valid solution
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if c in cols or (r + c ) in pos_diagonals or (r - c) in neg_diagonals:
+                    # Cannot use this r, c position. So, continue
+                    continue
+                
+                # Update all the sets before placing the queen
+                cols.add(c)
+                pos_diagonals.add(r + c)
+                neg_diagonals.add(r - c)
+                board[r][c] = "Q"
+
+                # Make the recursive backtrack call
+                backtrack(r + 1)
+
+                # Undo all the changes we made to the set to clean up
+                # because we're backtracking to try other combos/solutions
+                cols.remove(c)
+                pos_diagonals.remove(r + c)
+                neg_diagonals.remove(r - c)
+                board[r][c] = "."
+        
+        backtrack(0)
+        return res
