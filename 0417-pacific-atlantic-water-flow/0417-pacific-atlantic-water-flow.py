241369606ef4312b3a46b5ae6376d5ea66f7dec8
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-01 18:39:28 -0500

    Time: 39 ms (44.52%), Space: 19.4 MB (52.68%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,36 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # Idea is to start at the very edges and DFS into the board
-        # Two sets, atlantic and pacific, will keep track of which cells can access them
-        # and at the end, we'll return an intersection of the two sets
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        ROWS, COLS = len(heights), len(heights[0])
         atlantic, pacific = set(), set()
 
         def dfs(r, c, curr_set, prev_height):
-            # curr_set is the set we are currently working with
-            # prev_height will help us determine if we should continue recursing
-            # (we're going backwards, from ocean to inwards, so curr height must be >= prev_height)
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or heights[r][c] < prev_height or (r, c) in curr_set:
+            # Base case: r, c is out of bounds, is already in curr_set, or curr height is smaller than prev
+            if r < 0 or c < 0 or r >= len(heights) or c >= len(heights[0]) or (r, c) in curr_set or heights[r][c] < prev_height:
                 return
             
-            # add the current (r, c) coords to our current working set
             curr_set.add((r, c))
-
-            # then make recursive dfs calls in all 4 directions to continue adding to the set
             for dr, dc in directions:
-                curr_height = heights[r][c]
-                dfs(r + dr, c + dc, curr_set, curr_height)
+                dfs(r + dr, c + dc, curr_set, heights[r][c])
         
-        for r in range(ROWS):
+        for r in range(len(heights)):
+            # we want to dfs on the far left and right columns of the grid
             dfs(r, 0, pacific, heights[r][0])
-            dfs(r, COLS - 1, atlantic, heights[r][COLS - 1])
+            dfs(r, len(heights[0]) - 1, atlantic, heights[r][len(heights[0]) - 1])
         
-        for c in range(COLS):
+        for c in range(len(heights[0])):
+            # we wants to dfs on the top and bottom rows of the grid
             dfs(0, c, pacific, heights[0][c])
-            dfs(ROWS - 1, c, atlantic, heights[ROWS - 1][c])
-
-        # return the intersection between atlantic and pacific sets
-        # b/c those are the cells that can flow to both oceans
-        result = atlantic.intersection(pacific)
-        return list(result)
+            dfs(len(heights) - 1, c, atlantic, heights[len(heights) - 1][c])
+        
+        # compute the set intersection
+        # return the intersection as a list
+        res = atlantic.intersection(pacific)
+        return list(res)
