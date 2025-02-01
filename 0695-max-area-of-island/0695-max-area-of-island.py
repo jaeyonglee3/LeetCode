a9bd5dc392916b7e553637264c1deeb020207749
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-01 15:56:34 -0500

    Time: 16 ms (82.77%), Space: 18.4 MB (38.42%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,19 +2,17 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         res = 0
 
-        def dfs(r, c) -> int:
+        def dfs(r, c):
             if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == 0:
                 return 0
             
-            # Set current position to 0 so its not visited again
             grid[r][c] = 0
             return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
-
+        
         for row_num, row in enumerate(grid):
             for col_num, val in enumerate(row):
                 if val == 1:
-                    # Run DFS helper to calculate area of island
                     area = dfs(row_num, col_num)
                     res = max(res, area)
-        
+
         return res
