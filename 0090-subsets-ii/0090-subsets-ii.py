3131d82b8c82f512dc35930ccade45aca4c15309
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-02 00:24:21 -0500

    Time: 1 ms (34.64%), Space: 18 MB (45.94%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -7,16 +7,18 @@ class Solution:
         def dfs(i):
             if i >= len(nums):
                 res.append(subset[:])
-                return            
+                return
             
-            # Left branch, add nums[i]
+            # left branch, includes ALL SUBSETS that include nums[i]
             subset.append(nums[i])
             dfs(i + 1)
 
-            # right brach, do not add nums[i]
-            subset.pop()
-            while i < len(nums) - 1 and nums[i + 1] == nums[i]:
+            # right branch, includes all subsets that DO NOT include nums[i]
+            # since we cannot include a single nums[i], we need to skip any duplicates of nums[i]
+            # here before we make any recursive calls!!
+            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
                 i += 1
+            subset.pop()
             dfs(i + 1)
         
         dfs(0)
