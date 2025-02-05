96b5b3198d7a487c27c0e198749b6599da217f1d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-05 00:19:08 -0500

    Time: 0 ms (100%), Space: 17.7 MB (68.02%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -3,16 +3,19 @@ class Solution:
         # To use python heapq for a max heap
         # make all the values negative
         stones = [-weight for weight in stones]
-        heapq.heapify(stones)
+        heapq.heapify(stones) # this is a O(n) operation
 
         # Simulate the game as long as there are at least 2
         # stone weight remaining in the max heap
         while len(stones) >= 2:
             # stone y is guaranteed to have weight >= x
+            # but we made everything negative so we're guaranteed to have x >= y
             y = heapq.heappop(stones)
             x = heapq.heappop(stones)
 
             if x != y:
                 heapq.heappush(stones, -abs(y - x))
+            
+            # if stones are equal weights, do nothing!
         
         return abs(stones[0]) if len(stones) > 0 else 0
