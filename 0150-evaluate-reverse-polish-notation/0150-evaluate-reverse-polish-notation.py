8d197efdaa91580db5a81fcf57d60ab64d62f2ce
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-04 21:49:45 -0500

    Time: 4 ms (30.73%), Space: 19.1 MB (83.38%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,22 +1,20 @@
-import operator
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
         operators = {
             "+": operator.add,
             "-": operator.sub,
             "*": operator.mul,
-            "/": operator.truediv, # floordiv doesn't work because it'd round -0.4 to -1 instead of 0
+            "/": operator.truediv, # floordiv b/c division always truncates towards zero
         }
-        stack = []
-        
+
         for token in tokens:
-            if token in operators.keys():
-                rs = stack.pop()
-                ls = stack.pop()
+            if token in operators:
+                right = stack.pop()
+                left = stack.pop()
                 operation = operators[token]
-                new_val = operation(ls, rs)
-                stack.append(int(new_val)) # conversion to int truncates towards zero
+                result = operation(left, right)
+                stack.append(int(result))
             else:
                 stack.append(int(token))
         
