0d54cd9c888fca46f45db6e6dca95be9f6cd80e8
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-04 21:58:34 -0500

    Time: 185 ms (41.29%), Space: 36.9 MB (95.86%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,26 +1,20 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # Hold all fleets in a stack (nested array), each element is a fleet
-        # Iterate array in reverse. Calculate arrival time of each car (target-position)/speed
-        # If a car that is behind a car arrives sooner or at the same time, 
-        # the two must overlap and form a fleet at some point
-        # pop off the one travelling at a faster speed
-        # (fleets travel at the speed of the slowest car)
-
         fleets = []
+        # [(pos, speed), (pos, speed)]
         cars = list(zip(position, speed))
 
+        # go through the cars in reverse based on position
+        # so that we see the cars closer to target first
         for pos, speed in sorted(cars, reverse=True):
-            fleets.append((target-pos)/speed)
+            arrival_time = (target - pos) / speed
+            fleets.append(arrival_time)
 
-            # Check if car just added arrives sooner or 
-            # at same time than the car ahead of it
-            # but only check if >= 2 fleets currently
-            if len(fleets) >= 2 and fleets[-1] <= fleets[-2]:
+            # if car just added arrives sooner or at the same time
+            # as a car already in fleets (which is ahead of it)
+            # they must form a fleet at some point
+            # so just remove what you just added
+            while len(fleets) >= 2 and fleets[-2] >= fleets[-1]:
                 fleets.pop()
         
         return len(fleets)
-
-
-
-        
