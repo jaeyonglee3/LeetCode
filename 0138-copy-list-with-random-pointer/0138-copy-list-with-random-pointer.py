c011bc2ec16a03ee09b843b261825f4f7e801ba0
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-05 23:38:01 -0500

    Time: 38 ms (74.89%), Space: 18.7 MB (35.25%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,13 +9,19 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # None always maps to none, so no need to worry about trying to find the
+        # value when the key may be null
         old_to_new = {None : None}
 
+        # First, make a copy of every node and store it in old_to_new hashmap
+        # where the original node maps to the newly created copy
         curr = head
         while curr:
             old_to_new[curr] = Node(curr.val)
             curr = curr.next
         
+        # Then, iterate over each OG node again and for its counterpart in hashmap,
+        # assign its next and random pointers to the other values in the hashmap
         curr = head
         while curr:
             new_copy = old_to_new[curr]
@@ -24,4 +30,5 @@ class Solution:
             
             curr = curr.next
         
+        # Return the copy associated with the head
         return old_to_new[head]
