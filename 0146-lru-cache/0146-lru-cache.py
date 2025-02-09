cf2f6dab68442b688686ad50b9c29d5e1c1d6a62
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-09 00:08:42 -0500

    Time: 156 ms (40.06%), Space: 78.4 MB (51.59%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,39 +1,58 @@
 class Node:
     def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
 
 class LRUCache:
+    # Implemented with doubly linked list and hashmap
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = {}  # map key to node
-
+        self.cache = {}  # maps key to node, i.e. value is a ptr to node
+        
+        # Initialize dummy nodes for pointing to LRU and most recent
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
-
+    
     def remove(self, node):
+        # Remove from the list
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
-
+    
     def insert(self, node):
+        # Insert at right
         prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
+        # insert the node between prev and nxt
+        prev.next = node
+        nxt.prev  = node
         node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
         if key in self.cache:
+            # Must also update the node to most recent
+            # remove it, then reinsert it
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
+        
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
+        
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
         if len(self.cache) > self.cap:
+            # remove from linkedlist and delete/evict the LRU from hashmap (cache)
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
