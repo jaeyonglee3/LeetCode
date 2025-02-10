12c46333e1c55663733f1f0c2b7e7bc30ddea86d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-09 21:47:29 -0500

    Time: 3 ms (3.85%), Space: 18.2 MB (40.98%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,28 +1,22 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # first, find the row the target could fit into
         for row in matrix:
-            if row[-1] >= target:
-                # Perform binary search on that row
-                # Return true if found, false otherwise
-                return self.binSearch(row, target)
-        
-        # No row in the matrix can possibly contain target
-        return False
-    
-    def binSearch(self, row, target) -> bool:
-        l, r = 0, len(row) - 1
+            if row[0] <= target and target <= row[-1]:
+                # the target must be in this row
+                # perform a binary search on this row
+                l, r = 0, len(row) - 1
 
-        while r >= l:
-            mid = (r + l) // 2
+                while l <= r:
+                    mid = (l + r) // 2
 
-            if target == row[mid]:
-                return True
-            elif target > row[mid]:
-                l = mid + 1
-            else:
-                r = mid - 1
+                    if row[mid] == target:
+                        return True
+                    elif row[mid] > target:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                
+                return False
         
         return False
-            
-            
-        
