d87df6a73446a1617d3045624f261ccbd3ac0535
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-09 22:13:33 -0500

    Time: 226 ms (26.86%), Space: 22.2 MB (18.18%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -1,28 +1,36 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        def isPossible(capacity) -> bool:
-            curr_load = 0
-            days_taken = 1
+        def is_feasible(capacity: int) -> bool:
+            num_days = 1
+            total_weight = 0
 
             for weight in weights:
-                if curr_load + weight > capacity: # Then, ship load is at capacity!
-                    days_taken += 1
-                    curr_load = weight # Start a new load with the current weight
-                else:
-                    curr_load += weight # Add the weight to the current load
-            
-            return days_taken <= days
-        
-        l, r = max(weights), sum(weights)
-        res = sum(weights)
+                if num_days > days or weight > capacity:
+                    return False
 
-        while r >= l:
+                if total_weight + weight <= capacity:
+                    total_weight += weight
+                else:
+                    num_days += 1
+                    total_weight = weight
+            
+            return num_days <= days
+        
+        # define our search window for possible capacities
+        # min could be len(weights) * min(weights), could be feasible if all packages
+        # have the same weight as smallest weight
+        # max could be num packages * max(weights)
+        res = 0
+        l, r = 1, max(weights) * len(weights)
+
+        while l <= r:
             mid = (l + r) // 2
 
-            if isPossible(mid):
+            if is_feasible(mid):
                 res = mid
                 r = mid - 1
             else:
                 l = mid + 1
         
         return res
+
