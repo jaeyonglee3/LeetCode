33799829019d78c8f3264f61a5233442f0c0afc5
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-10 22:15:07 -0500

    Time: 47 ms (36.89%), Space: 18.3 MB (6.74%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,47 +6,20 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # brute force approach
-        # dfs on the root tree 
-        # check root, left, and right to see if they are the same
-
-        # implementing solution
-        # check first if root and subRoot are the same tree
-        # check if subRoot is a subtree of the left subtree by calling sametree fn on left subtree
-
-        # Edge cases
-        # - both trees are null, then yes they're same tree
-        # - 2 trees have same root, left, right, but one of them is taller, then not same tree
-        # - is root was null but subRoot was non empty, subRoot is not a subtree of root
-        # - is an empty subRoot tree a subtree of a non empty root? Yes (good clarification question for interview)
-
-        # Two of our edge cases
-        if not subRoot: return True
-        if not root: return False
+        def sameTree(p, q):
+            if not p and not q:
+                return True
+            if not p or not q or p.val != q.val:
+                return False
+            
+            return sameTree(p.left, q.left) and sameTree(p.right, q.right)
         
-        if self.sameTree(root, subRoot):
+        if not root:
+            return False
+        if sameTree(root, subRoot):
             return True
         
-        return (self.isSubtree(root.left, subRoot) or
-                self.isSubtree(root.right, subRoot))
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
-    def sameTree(self, root, subRoot):
-        # Alternate implementation of sameTree from Neetcode Video
-        # if not root and not subRoot:
-        #     return True
-
-        # # if both non-empty, we compare the trees
-        # if root and subRoot and root.val == subRoot.val:
-        #     return (self.sameTree(root.left, subRoot.left) and 
-        #             self.sameTree(root.right, subRoot.right))
-        
-        # return False  # One tree is empty while the other isn't, so return False
-
-        # Original implementation of sameTree by me from lc #100
-        if not root and not subRoot: return True
-        if not root or not subRoot: return False
-        if root.val != subRoot.val: return False
-
-        return (self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right))
 
         
