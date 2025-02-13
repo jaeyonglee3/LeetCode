a9563200be826c97ff0a7773b6caf58340861b74
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-13 00:13:39 -0500

    Time: 4 ms (12.44%), Space: 19.8 MB (29.05%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,19 +6,12 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def valid(node, lower_bound, upper_bound):  # left and right boundaries for the values of nodes
-            # Two Base cases:
-            # 1. Empty tree
-            # 2. We have a BST property violation
-
-            if not node: # empty tree is still valid BST
+        def dfs(root, lower, upper):
+            if not root:
                 return True
-            
-            if node.val >= upper_bound or node.val <= lower_bound:
+            if root.val <= lower or root.val >= upper:
                 return False
             
-            # check if left subtree is valid, lower bound doesnt change, upper bound is parent
-            # check if right subtree is valid, upper bound doesnt change, lower bound is parent
-            return valid(node.left, lower_bound, node.val) and valid(node.right, node.val, upper_bound)
-        
-        return valid(root, -math.inf, math.inf)
+            return dfs(root.left, lower, root.val) and dfs(root.right, root.val, upper)
+         
+        return dfs(root, -math.inf, math.inf)
