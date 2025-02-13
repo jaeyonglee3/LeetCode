393de4c0bd829bff10bdff91a7ba8f6eadbdcb38
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-12 22:55:36 -0500

    Time: 0 ms (100%), Space: 18 MB (16.82%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,26 +6,23 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # Apply level order traversal
-        # At each level, add node at end of the array to the final result array
-
         res = []
         q = collections.deque()
         q.append(root)
 
         while q:
             q_len = len(q)
-            curr_level = []
+            right_most = None
 
             for _ in range(q_len):
-                d = q.popleft()
+                curr_node = q.popleft()
 
-                if d:
-                    q.append(d.left)
-                    q.append(d.right)
-                    curr_level.append(d.val)
+                if curr_node:
+                    right_most = curr_node
+                    q.append(right_most.left)
+                    q.append(right_most.right)
             
-            if curr_level:
-                res.append(curr_level.pop())
-        
+            if right_most:
+                res.append(right_most.val)
+
         return res
