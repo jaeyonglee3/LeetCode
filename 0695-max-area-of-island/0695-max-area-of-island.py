ffdcdec822b04f7f1a9a105ebd9e66e883850894
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-16 18:32:44 -0500

    Time: 19 ms (74.84%), Space: 18.1 MB (78.37%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,7 +2,7 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         res = 0
 
-        def dfs(r, c):
+        def dfs(r, c) -> int:
             if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == 0:
                 return 0
             
@@ -13,6 +13,6 @@ class Solution:
             for col_num, val in enumerate(row):
                 if val == 1:
                     area = dfs(row_num, col_num)
-                    res = max(res, area)
-
+                    res = max(area, res)
+        
         return res
