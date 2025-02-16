46a5c1f13bfc76ba8cc8b519c1d9abd3393be5bc
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-15 23:29:38 -0500

    Time: 4 ms (77.7%), Space: 19.5 MB (64.48%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,21 +6,21 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        inorder_map = {val : idx for idx, val in enumerate(inorder)}
-
-        if not preorder or not inorder:
-            return
+        inorder_map = {val: idx for idx, val in enumerate(inorder)}  # Precompute inorder indices
         
-        new_node = TreeNode(preorder[0])
-        root_i = inorder_map[preorder[0]]
-
-        inorder_left = inorder[ : root_i]
-        inorder_right = inorder[root_i + 1 : ]
-
-        preorder_left = preorder[1 : len(inorder_left) + 1]
-        preorder_right = preorder[2 + len(inorder_left) - 1 : ]
-
-        new_node.left = self.buildTree(preorder_left, inorder_left)
-        new_node.right = self.buildTree(preorder_right, inorder_right)
-
-        return new_node
+        def helper(pre_start, pre_end, in_start, in_end):
+            if pre_start > pre_end or in_start > in_end:
+                return None
+            
+            root_val = preorder[pre_start]
+            root = TreeNode(root_val)
+            root_idx = inorder_map[root_val]  # O(1) lookup
+            
+            left_size = root_idx - in_start  # Number of nodes in left subtree
+            
+            root.left = helper(pre_start + 1, pre_start + left_size, in_start, root_idx - 1)
+            root.right = helper(pre_start + left_size + 1, pre_end, root_idx + 1, in_end)
+            
+            return root
+        
+        return helper(0, len(preorder) - 1, 0, len(inorder) - 1)
