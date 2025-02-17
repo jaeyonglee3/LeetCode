08ff62576d40481fab9c8f9622cb5a411882b9dd
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-17 15:10:39 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,36 +3,26 @@ class Solution:
         res = []
         nums.sort()
 
-        for i, i_val in enumerate(nums):
+        for i in range(len(nums)):
+            while i > 0 and i < len(nums) - 1 and nums[i - 1] == nums[i]:
+                i += 1
+
+            target = -nums[i]
             l, r = i + 1, len(nums) - 1
-            # Optimization: Skip duplicate numbers for the fixed 'x'
-            # if it isn't the first value in nums
-            # and if its the same num as the one before it, skip it!
-            if i > 0 and i_val == nums[i - 1]:  
-                continue 
 
             while r > l:
-                curr_sum = nums[l] + nums[r]
-                
-                if curr_sum == -i_val:
-                    res.append([i_val, nums[r], nums[l]])
-                    # e.g. [0, -2, -2, 0, 0, 2, 2]
-                    # Optimization: Skip duplicate numbers for 'l' and 'r'
-                    # Because its sorted, all duplicates would be next to each other!
-                    while l < r and nums[l] == nums[l + 1]:  
+                total = nums[l] + nums[r]
+                if total == target:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
                         l += 1
                     while l < r and nums[r] == nums[r - 1]:
                         r -= 1
                     l += 1
                     r -= 1
-                elif curr_sum > -i_val:
+                elif total > target:
                     r -= 1
                 else:
                     l += 1
         
         return res
-
-
-
-
-
