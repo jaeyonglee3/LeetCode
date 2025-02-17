7e73d547f062c33032ab8f633cf2c1cb76b070a8
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-17 13:56:10 -0500

    Time: 2 ms (91.3%), Space: 21.6 MB (73.27%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -6,20 +6,20 @@ class Solution:
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         ROWS, COLS = len(board), len(board[0])
 
+        # any O region that is not connected to an edge cell of the board
+        # will be turned into an X.
+        # dfs on any O edge cell to visit its entire region of connected Os
+        # and mark them with a temporary T. once that is done, make the changes to the board.
+        # all Ts will becomes Os again, and all Os will become Xs
+
         def dfs(r, c):
-            # Base Case
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] == "X" or board[r][c] == "T":
+            if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != 'O':
                 return
             
-            # Set the cell at (r, c) to some temporary character
-            board[r][c] = "T"
-
-            # Make recursive calls in all 4 directions
+            board[r][c] = 'T'
             for dr, dc in directions:
                 dfs(r + dr, c + dc)
         
-        # Invoke a dfs call on all the boarder cells
-        # to mark all unsurrounded regions (reverse thinking!)
         for r in range(ROWS):
             dfs(r, 0)
             dfs(r, COLS - 1)
@@ -28,11 +28,9 @@ class Solution:
             dfs(0, c)
             dfs(ROWS - 1, c)
         
-        # Now, iterate over entire board. Set all O's to X's and all T's to O's
-        # B/c at this point, T's are all the unsurrounded cells as marked by our DFS
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "T":
-                    board[r][c] = "O"
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
