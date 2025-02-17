0f05e3584643c949a4de8f89833348a53a9f66a2
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-17 13:40:52 -0500

    Time: 37 ms (48.1%), Space: 19.2 MB (80.32%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,28 +1,25 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        atlantic, pacific = set(), set()
+        ROWS, COLS = len(heights), len(heights[0])
+        pacific, atlantic = set(), set()
 
-        def dfs(r, c, curr_set, prev_height):
-            # Base case: r, c is out of bounds, is already in curr_set, or curr height is smaller than prev
-            if r < 0 or c < 0 or r >= len(heights) or c >= len(heights[0]) or (r, c) in curr_set or heights[r][c] < prev_height:
+        def dfs(r, c, prev_h, curr_set):
+            if r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in curr_set or heights[r][c] < prev_h:
                 return
             
+            # otherwise, that current r, c can reach that ocean
             curr_set.add((r, c))
             for dr, dc in directions:
-                dfs(r + dr, c + dc, curr_set, heights[r][c])
+                dfs(r + dr, c + dc, heights[r][c], curr_set)
         
-        for r in range(len(heights)):
-            # we want to dfs on the far left and right columns of the grid
-            dfs(r, 0, pacific, heights[r][0])
-            dfs(r, len(heights[0]) - 1, atlantic, heights[r][len(heights[0]) - 1])
+        for r in range(ROWS):
+            dfs(r, 0, -1, pacific)
+            dfs(r, COLS - 1, -1, atlantic)
         
-        for c in range(len(heights[0])):
-            # we wants to dfs on the top and bottom rows of the grid
-            dfs(0, c, pacific, heights[0][c])
-            dfs(len(heights) - 1, c, atlantic, heights[len(heights) - 1][c])
-        
-        # compute the set intersection
-        # return the intersection as a list
-        res = atlantic.intersection(pacific)
-        return list(res)
+        for c in range(COLS):
+            dfs(0, c, -1, pacific)
+            dfs(ROWS - 1, c, -2, atlantic)
+
+        result = pacific.intersection(atlantic)
+        return list(result)
