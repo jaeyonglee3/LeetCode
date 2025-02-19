f8f8ef6583de7a4591ebcbd8af315b9e659e80bf
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-18 23:04:58 -0500

    Time: 476 ms (82.55%), Space: 20.8 MB (23.79%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,27 +3,26 @@ class Solution:
         res = []
         nums.sort()
 
-        for i in range(len(nums)):
-            # we don't want to use the same value twice
-            if i > 0 and nums[i - 1] == nums[i]:
+        for i, i_val in enumerate(nums):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
+            
+            target = -i_val
 
-            target = -nums[i]
             l, r = i + 1, len(nums) - 1
-
             while r > l:
                 total = nums[l] + nums[r]
+
                 if total == target:
                     res.append([nums[i], nums[l], nums[r]])
-                    # the two while loops that skip duplicates for the l and r pointers must come 
-                    # right after a valid triplet is found (if total == target). Otherwise, they 
-                    # could accidentally skip over numbers that should be considered in the current iteration.
+
+                    while r > l and nums[l] == nums[l + 1]:
+                        l += 1
+                    while r > l and nums[r] == nums[r - 1]:
+                        r -= 1
+
                     l += 1
                     r -= 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
                 elif total > target:
                     r -= 1
                 else:
