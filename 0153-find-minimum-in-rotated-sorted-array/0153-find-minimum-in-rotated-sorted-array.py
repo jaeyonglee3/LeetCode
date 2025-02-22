28f0ad77a8386dbc28f297e40ee889faaa8c6176
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-22 13:05:06 -0500

    Time: 0 ms (100%), Space: 18.1 MB (32.16%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,6 +4,8 @@ class Solution:
         res = nums[0]
 
         if nums[-1] > nums[0]:
+            # Then, the entire array is still in ascending order
+            # meaning the min element is at the very front
             return res
         
         l, r = 0, len(nums) - 1
@@ -12,9 +14,15 @@ class Solution:
             mid = (r + l) // 2
 
             if nums[mid] >= nums[0]:
-                # Then, min element is in right half
+                # Then, mid is in the LEFT sorted portion
+                # The min element is in right half because right half contains all
+                # of the smaller elements
                 l = mid + 1
             else:
+                # Then, mid is in the RIGHT sorted portion
+                # result could be nums[mid] or would be to the LEFT of mid b/c
+                # anything to the right of mid would be greater than whats currently at mid.
+                # to look at left of mid, move right pointer to one beside mid to the left.
                 r = mid - 1
                 res = nums[mid]
         
