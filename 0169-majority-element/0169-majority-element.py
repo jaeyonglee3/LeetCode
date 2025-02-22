3bd35c9a204d4fa75e2ef4afa06019bb2d58713e
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-22 16:37:25 -0500

    Time: 7 ms (57.07%), Space: 19.3 MB (52.22%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        num_set = set(nums)
+        # the question guarantees nums to have at least 1
+        # element that occurs more than half the time in the array
+
+        result, count = nums[0], 0
+
+        for n in nums:
+            if count != 0:
+                if n == result:
+                    count += 1
+                else:
+                    count -= 1
+            else:
+                count = 1
+                result = n
         
-        for n in num_set:
-            if nums.count(n) > len(nums) // 2:
-                return n
+        return result
