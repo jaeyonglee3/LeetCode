6da6402af6caea1ca1309feadd728abfad8b9b6b
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-22 18:18:45 -0500

    Time: 139 ms (16.66%), Space: 26.9 MB (77.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-      # the day we buy must come before the day we sell
-      # profit = selling price - purchase price
+        # the day we buy must come before the day we sell
+        # profit = selling price - purchase price
 
-      res = 0
+        res = 0
 
-      # left will be the price at which we buy at
-      # right will be the selling price
-      l, r = 0, 1
+        # left will be the price at which we buy at
+        # right will be the selling price
+        l, r = 0, 1
 
-      while r < len(prices):
-        profit = prices[r] - prices[l]
-        res = max(res, profit)
+        while r <= len(prices) - 1:
+            curr_profit = prices[r] - prices[l]
+            res = max(curr_profit, res)
 
-        # move pointers based on result
-        if profit <= 0:
-          l = r
-        
-        r += 1
-      
-      return res
+            if curr_profit <= 0:
+                l = r
+            
+            r += 1
+    
+        return res
 
