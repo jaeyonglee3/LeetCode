834537497ce5d044143fc510f152840571512ea5
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-22 23:16:12 -0500

    Time: 0 ms (100%), Space: 17.8 MB (48.55%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,12 +1,23 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        # Start w/ empty result and an random word from the list
+        # we can use any word since the prefix must be common
+        # among all of the words anyway
         res = ""
+        arbitrary_word = strs[0]
 
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
+        # iterate over every character of our arbitrary word from our list
+        for i, c in enumerate(arbitrary_word):
+            # compare that character to the char from every
+            # other word at the same index
+            for word in strs:
+                if i == len(word) or word[i] != c:
+                    # then, we've found the prefix is no longer common
+                    # since it can't get any longer, return what we have
                     return res
             
-            res += strs[0][i]
+            # add to the result since loop finished and we can conclude
+            # the character is common in that positon with every other word
+            res += c
 
         return res
