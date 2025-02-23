5747b18e3af1934d8b38a0c9b45e6ce1dc8ba954
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-22 23:27:41 -0500

    Time: 0 ms (100%), Space: 18.6 MB (98.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit_total = 0
+        # Idea: It's sufficient to compare only adjacent prices because
+        # we can make multiple transactions. If the price increases from
+        # day i to day i+1, we can effectively simulate buying on day i
+        # and selling on day i+1 to capture that profit.
+        # This way, we maximize our profit from all upward price movements.
+
+        # i.e. simply capitalizing on every price increase is sufficient to maximize profit
+        res = 0
 
         for i in range(1, len(prices)):
             if prices[i] > prices[i - 1]:
-                profit_total += prices[i] - prices[i - 1]
+                res += prices[i] - prices[i - 1]
         
-        return profit_total
-
-
-        
+        return res
