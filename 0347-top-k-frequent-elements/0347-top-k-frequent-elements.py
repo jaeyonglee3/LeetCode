87d4d66e25cbde4e1b8f78793190e999116cca2e
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-22 21:57:06 -0500

    Time: 7 ms (52.81%), Space: 20.8 MB (99.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,22 +1,9 @@
-from queue import PriorityQueue
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # use a frequency map to map value to frequency
-        # extract the k values with highest frequency
-        res = []
-        freq_map = defaultdict(lambda: 0)
+        freq_map = collections.defaultdict(int)
 
-        for num in nums:
-            freq_map[num] += 1
-
-        # Throw the numbers into a priority queue
-        # Priority is highest frequency
-        p_queue = PriorityQueue()
-        for pair in freq_map.items():
-            p_queue.put((-pair[1], pair[0]))
-
-        for _ in range(k):
-            res.append(p_queue.get()[1])
+        for n in nums:
+            freq_map[n] += 1
         
-        return res
+        freq_elements = sorted(freq_map.keys(), key=lambda val: freq_map[val], reverse=True)
+        return freq_elements[:k]
