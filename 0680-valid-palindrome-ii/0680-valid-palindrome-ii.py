6295b445d5b7b570598608cf4d6588a9f7f713a4
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-23 23:37:30 -0500

    Time: 60 ms (62.7%), Space: 18.1 MB (62.64%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,32 +1,19 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        del_used = False
-        l, r = 0, len(s) - 1
-
-        def is_palindrome(s):
-            l, r = 0, len(s) - 1
-
-            while r >= l:
+        def is_palindrome(l, r):
+            while l < r:
                 if s[l] != s[r]:
                     return False
                 l += 1
                 r -= 1
             return True
 
-        while r >= l:
+        l, r = 0, len(s) - 1
+        while l < r:
             if s[l] != s[r]:
-                if r - l == 1:
-                    return True
-                elif is_palindrome(s[l + 1 : r + 1]) and not del_used:
-                    l += 1
-                    del_used = True
-                elif is_palindrome(s[l : r]) and not del_used:
-                    r -= 1
-                    del_used = True
-                else:
-                    return False
-            else:
-                l += 1
-                r -= 1
-        
+                return (is_palindrome(l + 1, r) or 
+                        is_palindrome(l, r - 1))
+            l += 1
+            r -= 1
+
         return True
