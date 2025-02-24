c49436539fa2685ef48e17c823bff853047428b6
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-23 22:00:47 -0500

    Time: 7 ms (49.59%), Space: 19.4 MB (42.75%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,20 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # step 1: build adjacency list representation of directed graph with
+        # The key insight here is that if there is a cycle in the prerequisite graph, it is 
+        # impossible to finish all courses, because a cycle means there is a circular 
+        # dependency. Thus, the problem reduces to detecting cycles in a directed graph.
+        
+        # Step 1: build adjacency list representation of directed graph with
         # vertices 0 -> numCourses - 1 and edges: prerequisites
         graph = collections.defaultdict(list)
         for a, b in prerequisites:
             graph[a].append(b)
         
-        # step 2: detect a cycle - if a cycle is detected, it is
+        # Step 2: detect a cycle - if a cycle is detected, it is
         # not possible to take all of the courses
         visited = set()
         def dfs_is_cycle_detected(node, path):
             if node in path:
+                # Then the node is already in the current recursion path, meaning
+                # a cycle exists!
                 return False
             if node in visited or graph[node] == []:
-                # The node has no neighbours
-                # it is a standalone node and therefore cannot have any cycles
+                # Either the node has already been process and no cycle was detected
+                # or the node has no neighbours, meaning no cycle possible
                 return True
             
             path.add(node)
@@ -22,7 +28,7 @@ class Solution:
                 if not dfs_is_cycle_detected(n, path):
                     return False
             
-            path.remove(node)
+            path.remove(node)  # Backtracking step, required in e.g. like: [[1, 7], [7, 0], [1, 0]]
             visited.add(node)
             return True
         
@@ -31,3 +37,6 @@ class Solution:
                 return False
         
         return True
+
+        # time: O(V + E) b/c each node and each edge is visited once
+        # space: O(V + E) b/c visited and path sets are O(V) and adjacency list is O(E)
