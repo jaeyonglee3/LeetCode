8a27d1c7fa2f171e986d04ea167e13db3722020c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-24 21:44:33 -0500

    Time: 0 ms (100%), Space: 19 MB (30.5%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,21 +1,15 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        seen = set()
-        i = 0
+        l = 1  # this pointer tells us where to put the unique value that we run into
 
-        while i < len(nums):
-            if nums[i] in seen:
-                j = i + 1
-                while j < len(nums) and nums[j] in seen:
-                    j += 1
-
-                if j == len(nums):
-                    # No more unique elements can be found in nums
-                    return len(seen)
-                
-                nums[i] = nums[j]
-                seen.add(nums[j])
-            else:
-                seen.add(nums[i])
-
-            i += 1
+        # right pointer looks for unique elements by checking if nums[r] != nums[r - 1]
+        # (this works because the input array is sorted)
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r - 1]:
+                # a new unique element is found so set the value at l ptr to val at r ptr
+                # and increment left ptr by one since we've put a unique value there
+                nums[l] = nums[r]
+                l += 1
+        
+        # at the end, the left ptr stops one index to the right of the last unqiue element
+        return l
