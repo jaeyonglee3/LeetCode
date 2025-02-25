5d3a7535b3f1120760014fa15de1d6bd5c412139
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-24 22:05:59 -0500

    Time: 4 ms (50.16%), Space: 17.9 MB (26.85%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -2,32 +2,32 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         ROWS, COLS = len(grid), len(grid[0])
-        q = collections.deque()  # holds (r, c) positions of rotten oranges only
-        fresh = 0
+        q = collections.deque()
+        fresh_count = 0
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
                     q.append((r, c))
                 elif grid[r][c] == 1:
-                    fresh += 1
-
+                    fresh_count += 1
+        
         minutes = 0
-        while q and fresh != 0:
+        while q and fresh_count > 0:
             for _ in range(len(q)):
                 r, c = q.popleft()
 
-                # Visit all the neighbouring cells and turn any fresh oranges rotten
                 for dr, dc in directions:
                     new_r, new_c = r + dr, c + dc
 
-                    if new_r < 0 or new_c < 0 or new_r == ROWS or new_c == COLS or grid[new_r][new_c] != 1:
+                    if min(new_r, new_c) < 0 or new_r == ROWS or new_c == COLS or grid[new_r][new_c] != 1:
                         continue
                     
+                    # this new cell we are exploring contains a fresh that must be turned rotten
                     grid[new_r][new_c] = 2
-                    fresh -= 1
                     q.append((new_r, new_c))
-            
+                    fresh_count -= 1
+
             minutes += 1
         
-        return minutes if fresh == 0 else -1
+        return minutes if fresh_count == 0 else -1
