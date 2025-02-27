b95e0a43e5e72c43144ba593ebfde44257c406f8
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-02-26 22:45:48 -0500

    Time: 171 ms (33.12%), Space: 51.4 MB (54.55%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -7,6 +7,10 @@ class Solution:
         for start, end in intervals[1:]:
             if start < prev_end:
                 # overlap detected
+                # it makes sense to "delete" the one with the bigger range because
+                # that one is more likely to overlap with other intervals.
+                # we set prev_end to min of both ending values.
+                # the interval that is "wider," i.e. had bigger end value, is effectively ignored
                 prev_end = min(end, prev_end)
                 res += 1
             else:
