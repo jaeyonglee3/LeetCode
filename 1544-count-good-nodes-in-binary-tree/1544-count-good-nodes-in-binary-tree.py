777305e52b64bdf605e018f9a64eb7327084323e
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-01 00:21:07 -0500

    Time: 134 ms (79.76%), Space: 32.2 MB (33.79%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -6,16 +6,13 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        # the root node is always good
-        def dfs(node, max_val):
-            # implement here
+        def dfs(node, greatest):
             if not node:
                 return 0
             
-            if node.val >= max_val:
+            if node.val >= greatest:
                 return 1 + dfs(node.left, node.val) + dfs(node.right, node.val)
             else:
-                return 0 + dfs(node.left, max_val) + dfs(node.right, max_val)
+                return 0 + dfs(node.left, greatest) + dfs(node.right, greatest)
         
         return 1 + dfs(root.left, root.val) + dfs(root.right, root.val)
-        
