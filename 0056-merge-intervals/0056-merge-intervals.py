21b726f82e89409e1ec3371dc2db68b14696e351
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-04 23:33:11 -0500

    Time: 11 ms (31.05%), Space: 21 MB (62.54%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -10,15 +10,9 @@ class Solution:
 
         # Step 3: Iterate through the sorted intervals starting from the second one.
         for i in range(1, len(intervals)):
-            if intervals[i][0] <= res[-1][1]:
-                # The current interval overlaps with the last interval added to res.
-                # Merge them by updating res[-1].
-                # its not necessary to update res[-1][0] bcuz we sorted the intervals array
-                # at the start. whats already in the result is guaranteed to have a smaller,
-                # or equal starting value.
-                res[-1][1] = max(intervals[i][1], res[-1][1])
+            if res[-1][1] >= intervals[i][0]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
             else:
-                # They do not overlap, so add the current interval to the result list.
                 res.append(intervals[i])
         
         return res
