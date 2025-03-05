6f178ccbebcc1458eb041e23d9c39746741d4763
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-05 18:56:18 -0500

    Time: 195 ms (17.83%), Space: 51.4 MB (54.73%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,19 +1,19 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # step 1: sort the intervals arr by start values so that any
+        # overlapping intervals are next to each other.
         intervals.sort()
         res = 0
-        prev_end = intervals[0][1]
+        curr_end = intervals[0][1]
 
-        for start, end in intervals[1:]:
-            if start < prev_end:
-                # overlap detected
-                # it makes sense to "delete" the one with the bigger range because
-                # that one is more likely to overlap with other intervals.
-                # we set prev_end to min of both ending values.
-                # the interval that is "wider," i.e. had bigger end value, is effectively ignored
-                prev_end = min(end, prev_end)
+        for i in range(1, len(intervals)):
+            # an overlap occurs between two intervals when one of their start times
+            # is lesser than the other's end time.
+            if intervals[i][0] < curr_end:
                 res += 1
+                # take the min since its less likely to overlap with other intervals.
+                curr_end = min(curr_end, intervals[i][1])
             else:
-                prev_end = end
+                curr_end = intervals[i][1]
         
         return res
