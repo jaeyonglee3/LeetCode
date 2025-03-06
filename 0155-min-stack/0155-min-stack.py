ff232ab291db1c33f405c246dcc3cd99e7e52332
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-05 23:55:00 -0500

    Time: 20 ms (11.02%), Space: 21.9 MB (6.86%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,10 +2,13 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        # last min keeps track of min val at that point in time in the stack
         self.last_min = math.inf
 
     def push(self, val: int) -> None:
         self.last_min = min(self.last_min, val)
+        # append with last_min val so we always know
+        # the min val at that point of the stack
         self.stack.append((val, self.last_min))
 
     def pop(self) -> None:
@@ -13,6 +16,9 @@ class MinStack:
         if not self.stack:
             self.last_min = math.inf
         else:
+            # set the last min to the min it was when
+            # stack[-1] was at the top of the stack
+            # because now its back at the top!
             self.last_min = self.stack[-1][1]
 
     def top(self) -> int:
