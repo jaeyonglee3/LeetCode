6c5904402ea6223d1b296306afaab91c2b6a0997
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-05 21:26:21 -0500

    Time: 117 ms (50.69%), Space: 33.2 MB (34.48%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,16 +2,14 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         # monotonically decreasing stack problem!
         res = [0] * len(temperatures)
-        stack = []
-        
-        # Iterate over all the temperatures
+        stack = []  # contains (index, temp) pairs
+
         for i, temp in enumerate(temperatures):
-            # First, check if what we are about to add
-            # is greater than whats at the top of the stack
             while stack and temp > stack[-1][1]:
-                index_to_update = stack.pop()[0]
-                res[index_to_update] = i - index_to_update
+                removed_i = stack.pop()[0]
+                res[removed_i] = i - removed_i
             
             stack.append((i, temp))
         
         return res
+
