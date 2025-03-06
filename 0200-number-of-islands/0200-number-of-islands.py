953d25aec682ad1a6a0952daa940cdda4085e9c6
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-05 20:14:11 -0500

    Time: 249 ms (58.08%), Space: 20.1 MB (82.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        ROWS, COLS = len(grid), len(grid[0])
+        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         res = 0
 
-        def dfs(r, c) -> None:
-            if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == "0":
+        # visit every cell. If we have a 1, run a DFS on it that would visit all
+        # adjacent 1s, marking them as zeros so as they are not visited again.
+        # once this is complete, increment res += 1 since we've successfully visited
+        # just one island. All separate islands remain intact.
+
+        def dfs(r, c):
+            # the purpose of this dfs is to mark grid[r][c] and all adjacent 1s as 0s
+            # so that they're not visited again and mistakenly counted as separate, unqiue islands.
+            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == "0":
                 return
             
             grid[r][c] = "0"
-
-            # call dfs in 4 directions (above, below, left, right)
-            # relative to the current row, col position
-            for dr, dc in directions:
+            for dr, dc in DIRECTIONS:
                 dfs(r + dr, c + dc)
             
             return
+            
         
-        # Iterate through every cell and call dfs() on each one
-        for row_num, row in enumerate(grid):
-            for col_num, val in enumerate(row):
-                if val == "1":
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
                     res += 1
-                    dfs(row_num, col_num)
+                    dfs(r, c)
         
-        # simply return the result
         return res
