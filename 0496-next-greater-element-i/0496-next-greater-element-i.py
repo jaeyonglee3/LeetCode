ee952ad42e0bf3840028b9caef741a63e53eb0c9
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-06 19:34:32 -0500

    Time: 0 ms (100%), Space: 18.1 MB (40.52%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,13 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         ans = [-1] * len(nums1)
+        nums1_set = set(nums1)
         num1_map = {n : i for i, n in enumerate(nums1)}
         stack = []
 
         for n in nums2:
             while stack and stack[-1] < n:
                 removed = stack.pop()
-                if removed in num1_map:
+                if removed in nums1_set:
                     ans[num1_map[removed]] = n
             
             stack.append(n)
