0f6aac1d990808f1b25e1f73407a9abf83f4be01
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-06 19:32:11 -0500

    Time: 31 ms (13.01%), Space: 17.9 MB (89.68%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,19 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # monotonically decreasing stack problem, solves in O(n1 + n2) time
-        res = [-1] * len(nums1)
+        ans = [-1] * len(nums1)
         stack = []
-        nums1_map = {val : i for i, val in enumerate(nums1)}
 
-        for i, num2 in enumerate(nums2):
-            while stack and num2 > stack[-1]:
+        for n in nums2:
+            while stack and stack[-1] < n:
                 removed = stack.pop()
-
-                # constant time lookup! O(1)
-                # instead of O(n1) lookup with "removed in nums1"
-                if removed in nums1_map:
-                    res[nums1_map[removed]] = num2
+                if removed in nums1:
+                    ans[nums1.index(removed)] = n
             
-            stack.append(num2)
+            stack.append(n)
         
-        return res
+        return ans
