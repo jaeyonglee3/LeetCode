9d48a99ab75d3205c4b258ef8b16c2cdc7a0e229
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-08 19:50:49 -0500

    Time: 0 ms (100%), Space: 18.2 MB (14.88%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,25 +1,25 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
+        # use a boolean array to keep track of numbers used so far
+        visited = [False] * len(nums)
 
-        def dfs(perm):
-            # Base case: the permutation contains all the numbers from nums
-            # at least once at this point.
-            if len(perm) == len(nums):
+        def dfs(i, perm):
+            if i == len(nums):
                 res.append(perm.copy())
                 return
-            
-            for i in range(len(nums)):
-                # if the number is already in the permutation, skip.
-                if nums[i] in perm:
-                    continue
-                
-                perm.append(nums[i])
-                dfs(perm)
-                perm.pop()  # pop from permutation to recursively backtrack
-            
-            # In Python, if a function reaches the end without an 
-            # explicit return statement, it implicitly returns None.
-        
-        dfs([])
+          
+            # Iterate over the nums list to create permutations
+            for j in range(len(nums)):
+                # Check if the number at index j is already in the current permutation
+                if not visited[j]:
+                    # Mark it as visited and add to current permutation
+                    visited[j] = True
+                    perm[i] = nums[j]
+                    dfs(i + 1, perm)
+                    
+                    # Backtrack step: unmark the number at index j as visited for the next iteration
+                    visited[j] = False
+
+        dfs(0, [0] * len(nums))
         return res
