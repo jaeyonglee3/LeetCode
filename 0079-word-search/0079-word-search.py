ba7d70a773f22e13d4d42d4a834f8ae40acfa4b3
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-10 20:51:07 -0400

    Time: 5058 ms (29.17%), Space: 18 MB (21.5%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        DIRECTIONS = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         ROWS, COLS = len(board), len(board[0])
-        path = set()
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        def dfs(i, r, c):
-            # Base case
-            if i >= len(word):
+        
+        def dfs(i, r, c, path):
+            if i > len(word) - 1:
                 return True
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] != word[i] or (r, c) in path:
+            if r < 0 or c < 0 or r == ROWS or c == COLS or word[i] != board[r][c] or (r, c) in path:
                 return False
             
-            # DFS body
             path.add((r, c))
-            for dr, dc in directions:
-                if dfs(i + 1, r + dr, c + dc):
+            for dr, dc in DIRECTIONS:
+                if dfs(i + 1, r + dr, c + dc, path):
                     return True
             
             path.remove((r, c))
             return False
-        
-        for row_num, row in enumerate(board):
-            for col_num in range(COLS):
-                if dfs(0, row_num, col_num):
+            
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(0, r, c, set()):
                     return True
         
         return False
