f3bf809ec833fec3decbe4b9463c6b256491c76f
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-15 16:43:10 -0400

    Time: 0 ms (100%), Space: 18.1 MB (77.4%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.py
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.py
@@ -1,17 +1,27 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
-        res = nums[0]  # Initialize res with the first element
+        res = nums[0]
       
-        while l <= r:
+        while r >= l:
             mid = (l + r) // 2
-            res = min(res, nums[mid])  # Update res with the current minimum value
+            curr_val = nums[mid]
+            res = min(res, curr_val)
           
-            if nums[mid] > nums[r]:  # Minimum is in the right half
+            if curr_val > nums[r]:
+                # Check towards right of curr_val
+                # Smaller numbers clearly exist towards the right of curr_val
                 l = mid + 1
-            elif nums[mid] < nums[r]:  # Minimum is in the left half
+            elif curr_val < nums[r]:
+                # Check towards left of curr_val
+                # we've either just visited the min, or there are more values
+                # belonging to the right sorted section that we need to check
                 r = mid - 1
             else:
-                r -= 1  # Handle duplicates by shrinking search space
+                # Handle duplicates by shrinking search space
+                # Since we have a duplicate, either curr_value itself is the min value
+                # or something else all. What we know for sure is that theres no need to
+                # keep nums[r] in the search range/window
+                r -= 1  
       
-        return res  # Return the tracked minimum value
+        return res
