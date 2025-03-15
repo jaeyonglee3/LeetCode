764a1682a52408c187a53d5093dda7dbb017e368
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-15 11:03:57 -0400

    Time: 0 ms (100%), Space: 18.4 MB (5.37%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,23 +1,38 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # 1) iterate over the rows and find the row that target could fit in
-        # 2) do an iterative binary search on that row and return T or F
+        ROWS, COLS = len(matrix), len(matrix[0])
 
-        for row in matrix:
-            if row[0] <= target and row[-1] >= target:
-                l, r = 0, len(row) - 1
+        top, bot = 0, ROWS - 1
+        is_row_found = False
 
-                while r >= l:
-                    mid = (r + l) // 2
-                    curr_val = row[mid]
+        while bot >= top:
+            row_idx = (top + bot) // 2
+            row = matrix[row_idx]
+            
+            if target > row[-1]:
+                top = row_idx + 1
+            elif target < row[0]:
+                bot = row_idx - 1
+            else:
+                is_row_found = True
+                break
+        
+        if not is_row_found:
+            return False
+        
+        row = matrix[(top + bot) // 2]
+        l, r = 0, COLS - 1
 
-                    if curr_val == target:
-                        return True
-                    elif curr_val > target:
-                        r = mid - 1
-                    else:
-                        l = mid + 1
-                
-                return False
+        while r >= l:
+            mid = (l + r) // 2
+            curr_val = row[mid]
+
+            if curr_val == target:
+                return True
+            elif curr_val > target:
+                r = mid - 1
+            else:
+                l = mid + 1
         
         return False
+                
