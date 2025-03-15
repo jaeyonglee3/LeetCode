f38abdd8f8bceadc8d5b493fff49017cdc4ab273
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-14 22:39:38 -0400

    Time: 0 ms (100%), Space: 17.8 MB (41.73%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,19 +1,20 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        letters = {2 : "abc", 3 : "def", 4 : 'ghi', 5 : "jkl", 6 : "mno", 7 : "pqrs", 8 : "tuv", 9 : "wxyz"}
-        res = []
-
-        def dfs(i, curr_combo):
-            # Base case
-            if i > len(digits) - 1:
-                if curr_combo != "": res.append(curr_combo[:]) 
-                return
-
-            curr_letters = letters[int(digits[i])]
-            for char in curr_letters:
-                curr_combo += char
-                dfs(i + 1, curr_combo)
-                curr_combo = curr_combo[:-1]
+        if not digits: return []
         
-        dfs(0, "")
+        res = []
+        mappings = {"2" : "abc", "3" : "def", "4" : "ghi", "5" : "jkl", 
+                    "6" : "mno", "7" : "pqrs", "8" : "tuv", "9" : "wxyz"}
+
+        def dfs(curr_string, i):
+            if i == len(digits):
+                res.append("".join(curr_string))
+                return
+            
+            for c in mappings[digits[i]]:
+                curr_string.append(c)
+                dfs(curr_string, i + 1)
+                curr_string.pop()
+        
+        dfs([], 0)
         return res
