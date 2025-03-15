9273ff0af0291a1afd1d53907585e3b447918cf7
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-15 13:10:15 -0400

    Time: 0 ms (100%), Space: 17.9 MB (81%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,29 +1,21 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # O(log n) time + input is sorted = binary search
         res = nums[0]
-
-        if nums[-1] > nums[0]:
-            # Then, the entire array is still in ascending order
-            # meaning the min element is at the very front
-            return res
         
-        l, r = 0, len(nums) - 1
+        # edge case: entire nums array is sorted
+        if nums[0] < nums[-1]: return res
 
+        l, r = 0, len(nums) - 1
         while r >= l:
             mid = (r + l) // 2
+            curr_val = nums[mid]
 
-            if nums[mid] >= nums[0]:
-                # Then, mid is in the LEFT sorted portion
-                # The min element is in right half because right half contains all
-                # of the smaller elements
-                l = mid + 1
-            else:
-                # Then, mid is in the RIGHT sorted portion
-                # result could be nums[mid] or would be to the LEFT of mid b/c
-                # anything to the right of mid would be greater than whats currently at mid.
-                # to look at left of mid, move right pointer to one beside mid to the left.
+            if curr_val < nums[0]:
+                # we are in the right sorted portion
                 r = mid - 1
-                res = nums[mid]
-        
+                res = curr_val
+            else:
+                # we are in the left sorted portion
+                l = mid + 1
+
         return res
