b369e1c82b2a0348d01e1cfc5dd23e6e0b331b96
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-15 12:38:50 -0400

    Time: 175 ms (30.22%), Space: 18.9 MB (76.54%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,32 +1,32 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # We need to find the smallest k such that koko can eat all bananas
-        # What is the search window for k?
-        # k is an element of [1, max(piles)]
-
-        # define helper isFeasible, returns bool given proposed k val
-        def is_doable(k):
-            time_total = 0
-            
-            for pile in piles:
-                if k > pile:
-                    time_total += 1
-                else:
-                    time_total += math.ceil(pile / k)
-            
-            return time_total <= h
-        
-        # Now, do a binary search on the window for k
-        l, r = 1, max(piles)
+        # try to identify an upper & lower bound on k
+        # and with those, try and find the minimum, valid k value
+        # valid meaning with that speed, koko can eat all bananas within h hours
         res = 0
 
-        while r >= l:
-            curr_k = (l + r) // 2
+        def is_feasible(k):
+            # returns True if koko can eat all bananas within h hours
+            # eating at a speed of k. Returns False otherwise.
+            hrs_required = 0
+
+            for pile in piles:
+                if pile < k:
+                    hrs_required += 1
+                else:
+                    hrs_required += math.ceil(pile / k)
             
-            if is_doable(curr_k):
-                r = curr_k - 1
-                res = curr_k
+            return hrs_required <= h
+        
+        # the fastest she can go is eating is the entire pile in the hour!
+        l, r = 1, max(piles)
+
+        while r >= l:
+            mid = (l + r) // 2
+            if is_feasible(mid):
+                res = mid
+                r = mid - 1
             else:
-                l = curr_k + 1
+                l = mid + 1
         
         return res
