1a6c3142026f1712f1cd2afb8e3c03049c184d51
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-15 21:03:16 -0400

    Time: 0 ms (100%), Space: 18 MB (67.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,27 +1,44 @@
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # O(log n) complexity + sorted input -> binary search
-        # we will be halving the search window size each time
-        # b/c the array is sorted and rotated, there are two sorted portions, left and right
-        # we can leverage binary search to determine which portion the target may lie in
+        # Goal: O(logn) -> Binary search
 
+        # [4,5,6,7,0,1,2]
+        # mid = 7
+
+        # [8, 9, 10, 0, 1, 2, 3, 4, 5]
+
+        # set up l, r ptrs to start at the opposite ends of nums
+        # as long as r >= l, calculate a mid index
+
+        # if that mid value is part of the left section
+            # if target < mid val and is >= nums[l]: search left of mid
+            # otherwise: search right of mid
+        # mid value is in the right section
+            # if target > mid and is <= nums[r]: search right of mid
+            # otherwise: search left of mid
+        
         l, r = 0, len(nums) - 1
-        while r >= l:
-            mid = (r + l) // 2
 
-            if nums[mid] == target:
+        while r >= l:
+            mid = (l + r) // 2
+            mid_val = nums[mid]
+
+            if mid_val == target:
                 return mid
-            elif nums[mid] >= nums[0]:
-                # we are in the left sorted portion
-                if target >= nums[l] and target < nums[mid]:
-                    r = mid -1
+            elif mid_val >= nums[0]:
+                # we are in the left section
+                if target < mid_val and target >= nums[l]:
+                    r = mid - 1
                 else:
                     l = mid + 1
             else:
-                # we are in the right sorted portion
-                if nums[mid] < target and target <= nums[r]:
+                # we are in the right section
+                if target > mid_val and target <= nums[r]:
                     l = mid + 1
                 else:
                     r = mid - 1
-
+        
         return -1
