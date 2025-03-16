fdbe596d0d7760eac388fcc67151f94283cf5288
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-16 18:05:50 -0400

    Time: 166 ms (16.15%), Space: 74.8 MB (67.86%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,40 +1,32 @@
+from collections import defaultdict
+
 class TimeMap:
 
     def __init__(self):
-        self.timeMap = {}
+        self.dict = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.timeMap:
-            self.timeMap[key] = []
-
-        self.timeMap[key].append((value, timestamp))
+        self.dict[key].append((value, timestamp))
 
     def get(self, key: str, timestamp: int) -> str:
-        # optimization: if the key just isn't present, return an empty string
         res = ""
-        if key not in self.timeMap:
+        
+        if key not in self.dict: 
             return res
         
-        # Run a binary search on the value array to find
-        # the timestamp or the closest one
-        value_arr = self.timeMap[key]
-        l, r = 0, len(value_arr) - 1
-
-        if value_arr[0][1] > timestamp:
-            return res
-
-        # optimization: if even the smallest timestamp is too big,
-        # return an empty string
+        values = self.dict[key]
+        l, r = 0, len(values) - 1
+        
         while r >= l:
             mid = (l + r) // 2
-            
-            if value_arr[mid][1] == timestamp:
-                return value_arr[mid][0]
-            elif value_arr[mid][1] > timestamp:
-                r = mid - 1
-            else: # the value is smaller than timestamp so still valid
+
+            if values[mid][1] == timestamp:
+                return values[mid][0]
+            elif values[mid][1] < timestamp:
+                res = values[mid][0]
                 l = mid + 1
-                res = value_arr[mid][0]
+            else:
+                r = mid - 1
         
         return res
 
@@ -42,3 +34,11 @@ class TimeMap:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+# Main ds: dictionary, where keys are the keys and the values are arrays containing multiple values for each key
+# as tuples of (value, timestamp)
+
+# set method: will simply add a new entry to our dictionary
+
+# get method: will use the given key to fetch the array of associated values from our dictionary
+# and use binary search to fetch the value equal to the timestamp or closest to it without going over.
