32c1ac0699670ad3414e717b323a3115a8099935
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-16 23:22:29 -0400

    Time: 343 ms (54%), Space: 31.7 MB (32.88%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,38 +1,33 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        NUM_FLOWERS = len(bloomDay)
-        
-        def is_feasible(days: int) -> bool:
-            bouquets = 0
-            flowers = 0
+        # m bouqets with k flowers in each
+        # flowers must be adjacent in the bloomDay array
+        # want to find: the min num of days we need to make m bouqets
 
-            for bloom_day in bloomDay:
-                if bloom_day <= days:
-                    flowers += 1
-                else:
-                    flowers = 0
-                
-                if flowers == k:
-                    bouquets += 1
-                    flowers = 0
+        # [1,10,3,10,2]
+        # need 3 bouqets with 1 flower in each of them
+        # 3 days
+        def isFeasible(num_days) -> bool:
+            num_flowers, num_bouquets = 0, 0
             
-            return bouquets >= m
+            for day in bloomDay:
+                if day <= num_days:
+                    num_flowers += 1
+                else:
+                    num_flowers = 0
+                
+                if num_flowers == k:
+                    num_bouquets += 1
+                    num_flowers = 0
+            
+            return num_bouquets >= m
         
-        if NUM_FLOWERS < m * k:
-            # we do not have enough flowers
-            return -1
-
-        # define a search window
-        # its between min/max bloom days because the earliest possible day
-        # to make bouquets is when the fastest flower blooms, 
-        # and the latest is when the slowest one does.
         l, r = min(bloomDay), max(bloomDay)
         res = -1
+        while r >= l:
+            mid = (l + r) // 2
 
-        while l <= r:
-            mid = (r + l) // 2
-
-            if is_feasible(mid):
+            if isFeasible(mid):
                 res = mid
                 r = mid - 1
             else:
