bb1129f151a19662c4186e19d67287d678d10a0c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-16 21:43:45 -0400

    Time: 15 ms (85.32%), Space: 17.8 MB (62.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,24 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-
-        n = len(s1)
-        m = len(s2)
-        s1_map = Counter(s1)
-        s2_map = Counter()
-
-        for i in range(m):
-            s2_map[s2[i]] += 1
-            
-            if i >= n:
-                char_to_rmv = s2[i - n]
-                if s2_map[char_to_rmv] > 1:
-                    s2_map[char_to_rmv] -= 1                    
-                else:
-                    del s2_map[char_to_rmv]
-
-            if s1_map == s2_map:
-                return True 
+        freq = {}
+        for c in s1:
+            freq[c] = freq.get(c, 0) + 1
+        
+        freq_copy = freq.copy()
+        l = 0
+        for i in range(len(s2)):
+            if s2[i] in freq:
+                freq[s2[i]] -= 1
+                if freq[s2[i]] == 0 and i - l + 1 == len(s1):
+                    return True
+                while freq[s2[i]] < 0:
+                    freq[s2[l]] += 1
+                    l += 1            
+            else:
+                freq = freq_copy.copy()
+                l = i + 1
 
         return False
