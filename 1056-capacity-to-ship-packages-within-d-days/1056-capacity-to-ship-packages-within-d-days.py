64bc7d55830b35e3bf7732f013ff9341b0117ee3
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-17 22:24:28 -0400

    Time: 189 ms (58.58%), Space: 22.2 MB (55.37%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -1,35 +1,47 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        def is_feasible(capacity: int) -> bool:
-            num_days = 1
-            total_weight = 0
+        # want to find: smallest weight capacity x of the ship s.t. everything
+        # can be delivered within the number of days
+
+        # assumptions + clarifications
+        # - the ship makes 1 trip per day and each trip, its weight
+        # cannot exceed the capacity we set.
+        # - "loading in order" means from left to right in weights array
+
+        # weights = [1,2,3,4,5,6,7,8,9,10]
+        # days = 5
+        # output = 15
+
+        # 1. for a suggested x value, how do we determine its feasibility?
+        # helper fn that takes in x. it'll iterate over the packages and use counters
+        # to determine how many days it will take to ship everything.
+        def isFeasible(x) -> bool:
+            days_taken = 1
+            curr_load = 0
 
             for weight in weights:
-                if num_days > days:
-                    return False
-
-                if total_weight + weight <= capacity:
-                    total_weight += weight
+                if curr_load + weight <= x:
+                    curr_load += weight
                 else:
-                    num_days += 1
-                    total_weight = weight
+                    days_taken += 1  # 'sending' the ship off
+                    curr_load = weight
             
-            return num_days <= days
-        
-        # define search window
-        # min would be max(weights) bc ship should be able to ship >= 1 package
-        # max would be sum(weights) bc then we could ship ALL the packages in 1 day
-        res = 0
-        l, r = max(weights), sum(weights)
+            return days_taken <= days
 
-        while l <= r:
+
+        # 2. how do we suggest the right candidates for x?
+        # binary search approach -> define a search range of potential values for x
+        # and find the smallest, feasible one
+        l, r = max(weights), sum(weights)
+        res = 0
+
+        while r >= l:
             mid = (l + r) // 2
 
-            if is_feasible(mid):
+            if isFeasible(mid):
                 res = mid
                 r = mid - 1
             else:
                 l = mid + 1
         
         return res
-
