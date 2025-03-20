b346d13e75eb1c570a5e5911ec103cb650af448d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-20 10:45:36 -0400

    Time: 159 ms (51.85%), Space: 20.5 MB (62.25%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -3,9 +3,8 @@ class Solution:
         ROWS, COLS = len(mat), len(mat[0])
         DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        new_mat = [[1] * COLS for _ in range(ROWS)]
+        new_mat = [[-1] * COLS for _ in range(ROWS)]
         q = collections.deque()
-        visited = set()
 
         for r in range(ROWS):
             for c in range(COLS):
@@ -19,16 +18,15 @@ class Solution:
             for dr, dc in DIRS:
                 new_r, new_c = r + dr, c + dc
 
-                if min(new_r, new_c) < 0 or new_r == ROWS or new_c == COLS or new_mat[new_r][new_c] != 1 or (new_r, new_c) in visited:
+                if min(new_r, new_c) < 0 or new_r == ROWS or new_c == COLS or new_mat[new_r][new_c] != -1:
                     continue
                 
                 new_mat[new_r][new_c] = new_mat[r][c] + 1
-                visited.add((new_r, new_c))
                 q.append((new_r, new_c))
         
         return new_mat
 
         # time: O(m * n) b/c each cell is processed once
-        
-        # space: O(m * n) -> Visited set stores at most m * n entries. 
+
+        # space: O(m * n)
         # In the worst case, queue holds all m * n cells
