869a690e34976f057f2e39e58d0d6bcaa2aaf06c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-03-29 13:21:30 -0400

    Time: 0 ms (100%), Space: 18.9 MB (34.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # O(n^2) where n = len(nums)
+        # O(n) where n = len(nums)
 
-        for i, num_i in enumerate(nums):
-            for j, num_j in enumerate(nums):
-                if (i != j and num_i + num_j == target):
-                    return [i, j]
-        
+        seen = {}  # maps values to index in array
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in seen:
+                return [i, seen[diff]]
+            
+            seen[n] = i
