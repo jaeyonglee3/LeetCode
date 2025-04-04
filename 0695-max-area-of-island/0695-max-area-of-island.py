abb51607b316ba162e77f16ee93089670cf10d55
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-04 01:38:31 -0400

    Time: 27 ms (47.3%), Space: 18.2 MB (80.02%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,25 +3,16 @@ class Solution:
         res = 0
         ROWS, COLS = len(grid), len(grid[0])
 
-        # approach: iterate over every cell.
-        # if cell contains a 1, call dfs(r, c) on it and the dfs
-        # will return an int representing the area of the island
-        # formed by all 4 directionally adjacent cells with value 1
-        # connected to the cell from which we started at.
-
-        def dfs(r, c):
-            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == 0:
+        def dfs(r, c) -> int:
+            if min(r, c) < 0 or r == ROWS or c == COLS or grid[r][c] == 0:
                 return 0
             
-            # set the current position to 0 so its not counted again in the future
             grid[r][c] = 0
-            
             return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
         
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    curr_area = dfs(r, c)
-                    res = max(res, curr_area)
+                    res = max(dfs(r, c), res)
         
         return res
