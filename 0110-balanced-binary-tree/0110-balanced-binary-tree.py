f6a8175a2d7d438557ca6a9c37371911550364bf
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-14 23:56:43 -0400

    Time: 1 ms (77.66%), Space: 19 MB (26.01%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,20 +6,13 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        # optimal "bottom up" approach. start from the leaf nodes
-
-        # returns bool for whether node is balanced, and int height
         def dfs(node) -> (bool, int):
             if not node:
                 return (True, 0)
             
             left, right = dfs(node.left), dfs(node.right)
-            # if we find even a single unbalanced subtree, the entire tree is unbalanced
-            is_balanced = abs(left[1] - right[1]) <= 1 and left[0] and right[0]
+            is_balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
 
             return (is_balanced, 1 + max(left[1], right[1]))
         
         return dfs(root)[0]
-
-        # time: O(n) since each node is visited once
-        # space: O(n) worst case with a skewed tree, O(logn) best case with a balanced tree (recursive stack)
