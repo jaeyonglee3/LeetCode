9062fc69523ccd0216d20c7d2bee0ee771478f82
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-18 00:09:00 -0400

    Time: 4 ms (69.1%), Space: 19.4 MB (40.67%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,33 +1,42 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Cycle detection problem - if cycle detected, courses cannot all be finished
-        # Represent with adjacency list
-        courses = defaultdict(list)
-        for a, b in prerequisites:
-            courses[a].append(b)
+        # The key insight here is that if there is a cycle in the prerequisite graph, it is 
+        # impossible to finish all courses, because a cycle means there is a circular 
+        # dependency. Thus, the problem reduces to detecting cycles in a directed graph.
         
-        # Define a DFS helper that returns a bool based on whether
-        # all courses can be taken
+        # Step 1: build adjacency list representation of directed graph with
+        # vertices: {0 -> numCourses - 1} and edges: {prerequisites}
+        graph = collections.defaultdict(list)
+        for a, b in prerequisites:
+            graph[a].append(b)
+        
+        # Step 2: detect a cycle - if a cycle is detected, it is
+        # not possible to take all of the courses
         visited = set()  # prevents redundant DFS calls → avoids reprocessing nodes that have already been checked for cycles.
-        def dfs(node, path) -> bool:
+        def dfs_is_cycle_detected(node, path):
             if node in path:
-                return False  # Cycle detected
+                # Then the node is already in the current recursion path, meaning
+                # a cycle exists!
+                return False
             if node in visited:
+                # The node has already been processed and no cycle was detected
                 return True
             
             path.add(node)
-            for n in courses[node]:
-                valid = dfs(n, path)
-                if not valid:
+            for n in graph[node]:
+                if not dfs_is_cycle_detected(n, path):
                     return False
             
-            path.remove(node)
+            path.remove(node)  # Backtracking step, required in e.g. like: [[1, 7], [7, 0], [1, 0]]
             visited.add(node)
             return True
         
-        for node in range(numCourses):
-            valid = dfs(node, set())
-            if not valid:
+        # Call dfs on every node because the graph may be disconnected
+        for course in range(numCourses):
+            if not dfs_is_cycle_detected(course, set()):
                 return False
         
         return True
+
+        # time: O(V + E) b/c each node and each edge is visited once
+        # space: O(V + E) b/c visited and path sets are O(V) and adjacency list is O(E)
