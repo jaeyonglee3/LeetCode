efbf495cc8f2eda782a4e86b842bb48060790f3c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-18 23:06:23 -0400

    Time: 5 ms (36.69%), Space: 17.7 MB (71.99%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,20 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        # multi-source BFS approach
+        # implemented with a queue that always contains all rotten cell coordinates
+        # at each "minute" (iteration), we'll check 4-directionally from all cells in queue
+        # and handle them accordingly
         DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        
+        ROWS, COLS = len(grid), len(grid[0])
         q = collections.deque()
         num_fresh = 0
-        total_mins = 0
 
+        # step 1 - fill the queue with all rotten oranges
         for r in range(ROWS):
             for c in range(COLS):
+                if grid[r][c] == 2:
+                    q.append((r, c))
                 if grid[r][c] == 1:
                     num_fresh += 1
-                elif grid[r][c] == 2:
-                    q.append((r, c))
         
+        # step 2 - process each rotten orange, one at a time
+        # look 4 directionally from each one, making any adjacent fresh oranges rotten
+        num_minutes = 0
         while q and num_fresh:
+            num_minutes += 1
+            
             for _ in range(len(q)):
                 r, c = q.popleft()
 
@@ -25,9 +33,7 @@ class Solution:
                         continue
                     
                     grid[new_r][new_c] = 2
-                    num_fresh -= 1
                     q.append((new_r, new_c))
-                
-            total_mins += 1
+                    num_fresh -= 1
         
-        return total_mins if num_fresh == 0 else -1
+        return num_minutes if num_fresh == 0 else -1
