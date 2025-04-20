660e6c8b8f2db2a665b8c23bf721f7a9afd8fdee
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-19 22:50:18 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # DFS approach - call a dfs() helper on each cell that contains the starting letter
+        # run a dfs, keeping track of which char in the word is being looked for
+        # and also the current "path" of cell coordinates.
+        # if the last letter of the word is reached, we have a valid path of letters
+        # that together form the word, and therefore can return true.
         ROWS, COLS = len(board), len(board[0])
-        DIRECTIONS = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+        DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        def dfs(i, r, c, path):
-            if i > len(word) - 1:
-                return True
-            if min(r, c) < 0 or r == ROWS or c == COLS or board[r][c] != word[i] or (r, c) in path:
+        def dfs(r, c, i, path) -> bool:
+            if min(r, c) < 0 or r == ROWS or c == COLS or (r, c) in path or board[r][c] != word[i]:
                 return False
+            if i == len(word) - 1 and board[r][c] == word[i]:
+                return True
             
             path.add((r, c))
-            # Now, explore 4-directionally and make recursive calls
-            for dr, dc in DIRECTIONS:
-                if dfs(i + 1, r + dr, c + dc, path):
+            for dr, dc in DIRS:
+                if dfs(r + dr, c + dc, i + 1, path):
                     return True
             
-            # no matter how deep into the path we are, this is the same path variable
-            # from the letter we started with. Remove is necessary to reset the path
-            # back to normal after trying a bad path.
-            path.remove((r, c))
+            path.pop()
             return False
         
         for r in range(ROWS):
             for c in range(COLS):
-                if dfs(0, r, c, set()):
-                    return True
+                if board[r][c] == word[0]:
+                    if dfs(r, c, 0, set()):
+                        return True
         
         return False
