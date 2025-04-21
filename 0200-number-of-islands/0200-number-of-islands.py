55cee123cc2f219b23690ad7f5b8e588a3144ae7
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-21 00:18:40 -0400

    Time: 237 ms (79.68%), Space: 20.1 MB (82.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # iterate over the grid. each time a 1 is encountered, call a dfs() helper
+        # on it and pass its r, c coordinates. increment result by 1.
+        # dfs() helper responsible for turning that cell and all connected 1 cells
+        # into 0s to mark them as "visited," so that land cells that are part of
+        # a singular island are certainly only counted once
         ROWS, COLS = len(grid), len(grid[0])
-        DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        DIRS = [(1, 0), (-1, 0), (0, -1), (0, 1)]
         res = 0
 
         def dfs(r, c):
-            # base cases
             if min(r, c) < 0 or r == ROWS or c == COLS or grid[r][c] == "0":
                 return
             
             grid[r][c] = "0"
             for dr, dc in DIRS:
                 dfs(r + dr, c + dc)
-            
-            return
         
-        # visit every cell
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
