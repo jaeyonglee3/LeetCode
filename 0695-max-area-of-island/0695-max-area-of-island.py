98d1ebb37b542a0da33c97ac04b6e073cdba4498
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-24 00:44:43 -0400

    Time: 24 ms (52.44%), Space: 18.3 MB (68.05%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,18 +1,29 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        res = 0
+        # approach - we'll define a dfs helper that takes in a r, c position
+        # in the grid and performs a dfs starting from that cell. it will
+        # return the area of the island that includes the cell at r, c.
+        # it will also mark visited island cells as 0s so they're not visited again.
+        # we'll iterate over each cell in the grid and call dfs() on each island cell.
         ROWS, COLS = len(grid), len(grid[0])
+        DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        res = 0
 
         def dfs(r, c) -> int:
             if min(r, c) < 0 or r == ROWS or c == COLS or grid[r][c] == 0:
                 return 0
             
             grid[r][c] = 0
-            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+            area = 1
+            for dr, dc in DIRS:
+                area += dfs(r + dr, c + dc)
+
+            return area
         
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
-                    res = max(dfs(r, c), res)
+                    area = dfs(r, c)
+                    res = max(res, area)
         
         return res
