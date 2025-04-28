855f4b53d86da607d3b4c019dcb1e2c4785e4e87
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-28 16:35:45 -0400

    Time: 0 ms (100%), Space: 17.8 MB (41.82%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -5,16 +5,16 @@ class Solution:
         res = []
         mappings = {"2" : "abc", "3" : "def", "4" : "ghi", "5" : "jkl", 
                     "6" : "mno", "7" : "pqrs", "8" : "tuv", "9" : "wxyz"}
-
-        def dfs(curr_string, i):
-            if i == len(digits):
-                res.append("".join(curr_string))
+        
+        def dfs(i, curr):
+            if i > len(digits) - 1:
+                res.append("".join(curr))
                 return
             
             for c in mappings[digits[i]]:
-                curr_string.append(c)
-                dfs(curr_string, i + 1)
-                curr_string.pop()
+                curr.append(c)
+                dfs(i + 1, curr)
+                curr.pop()
         
-        dfs([], 0)
+        dfs(0, [])
         return res
