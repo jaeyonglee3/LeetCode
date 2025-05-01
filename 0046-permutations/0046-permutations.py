0084f4f9aff36caecfd03e1d32bd0a82c0735dc8
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-30 22:05:32 -0400

    Time: 0 ms (100%), Space: 18.2 MB (14.86%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,9 +1,8 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        visited = [False] * len(nums)
 
-        def dfs(i, curr):
+        def dfs(i, curr, visited):
             if i > len(nums) - 1:
                 res.append(curr[:])
                 return
@@ -14,10 +13,10 @@ class Solution:
                 
                 curr.append(nums[j])
                 visited[j] = True
-                dfs(i + 1, curr)
+                dfs(i + 1, curr, visited)
 
                 curr.pop()
                 visited[j] = False
         
-        dfs(0, [])
+        dfs(0, [], [False] * len(nums))
         return res
