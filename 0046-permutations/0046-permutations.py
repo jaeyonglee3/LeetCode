eaa6c9b5f6ed9304d01236b8f06e4f91b26fb552
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-04-30 22:03:57 -0400

    Time: 0 ms (100%), Space: 17.9 MB (57.15%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,20 +3,21 @@ class Solution:
         res = []
         visited = [False] * len(nums)
 
-        def dfs(i, curr_perm):
-            if i == len(nums):
-                res.append(curr_perm.copy())
+        def dfs(i, curr):
+            if i > len(nums) - 1:
+                res.append(curr[:])
                 return
             
             for j in range(len(nums)):
                 if visited[j]:
                     continue
                 
+                curr.append(nums[j])
                 visited[j] = True
-                curr_perm[i] = nums[j]
-                dfs(i + 1, curr_perm)
-                visited[j] = False
+                dfs(i + 1, curr)
 
+                curr.pop()
+                visited[j] = False
         
-        dfs(0, [0] * len(nums))
+        dfs(0, [])
         return res
