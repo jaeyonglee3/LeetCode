86540947953b2f4d00813430d8acbd1f16d46b68
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-02 01:34:58 -0400

    Time: 37 ms (46.53%), Space: 19.3 MB (64.18%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,8 +1,9 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        # the result contains all coordinates from which water can flow into both oceans
         ROWS, COLS = len(heights), len(heights[0])
         pacific, atlantic = set(), set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def dfs(r, c, prev_h, curr_set):
             if r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in curr_set or heights[r][c] < prev_h:
@@ -12,7 +13,7 @@ class Solution:
             curr_set.add((r, c))
             for dr, dc in directions:
                 dfs(r + dr, c + dc, heights[r][c], curr_set)
-        
+
         for r in range(ROWS):
             dfs(r, 0, -1, pacific)
             dfs(r, COLS - 1, -1, atlantic)
@@ -20,6 +21,9 @@ class Solution:
         for c in range(COLS):
             dfs(0, c, -1, pacific)
             dfs(ROWS - 1, c, -2, atlantic)
-
+        
         result = pacific.intersection(atlantic)
         return list(result)
+        
+        
+
