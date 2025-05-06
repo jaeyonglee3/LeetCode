a07b4a326668326a72a45d0d5dd8ad580e6fa491
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-05 23:33:31 -0400

    Time: 0 ms (100%), Space: 17.8 MB (97.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,20 +1,29 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        # Run a DFS from the starting cell to visit all adjacent cells
-        # if the cell we've visited has same pixel value as starting val, change its colour
-        ROWS, COLS = len(image), len(image[0])
-        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        start_val = image[sr][sc]
+        # Use a BFS approach - initialize a queue with the starting cell coordinates
+        # at each step of the BFS, popleft from the queue and explore all 4 of that cell's neighbors.
+        # If that cell's neighbors have the SAME color as the starting pixel, update that cell's pixel
+        # value too and add it to the queue to be processed
+        if image[sr][sc] == color: return image
 
-        def dfs(r, c):
-            # Base cases
-            if min(r, c) < 0 or r == ROWS or c == COLS or image[r][c] != start_val or image[r][c] == color:
-                return
-            
-            # Otherwise, we're at a cell for which we must change the colour
-            image[r][c] = color
-            for dr, dc in DIRECTIONS:
-                dfs(r + dr, c + dc)
+        ROWS, COLS = len(image), len(image[0])
+        DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         
-        dfs(sr, sc)
+        og_pixel = image[sr][sc]
+        q = collections.deque()
+        q.append((sr, sc))
+
+        while q:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                image[r][c] = color
+
+                for dr, dc in DIRS:
+                    nr, nc = r + dr, c + dc
+
+                    if min(nr, nc) < 0 or nr == ROWS or nc == COLS or image[nr][nc] != og_pixel:
+                        continue
+                        
+                    q.append((nr, nc))
+        
         return image
