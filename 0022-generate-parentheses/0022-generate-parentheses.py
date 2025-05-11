e58cf26800de0153515bb9262476ddbe18c8fc1c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-10 23:27:05 -0400

    Time: 0 ms (100%), Space: 17.9 MB (69.91%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,25 +1,24 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        # while forming combinations: 
+        # of closing parentheses can never exceed the number of opening.
+        # number of open parentheses can never exceed n.
         res = []
 
-        # well formed parentheses have an equal number of openings and closings
-        def backtrack(curr, num_open, num_close):
+        def dfs(curr, num_open, num_close):
             if num_open == num_close == n:
-                res.append("".join(curr))
+                res.append("".join(curr[:]))
                 return
             
-            # left branch, add opening bracket
             if num_open < n:
                 curr.append("(")
-                backtrack(curr, num_open + 1, num_close)
+                dfs(curr, num_open + 1, num_close)
                 curr.pop()
-
-            # right branch, add closing bracket
+            
             if num_close < num_open:
                 curr.append(")")
-                backtrack(curr, num_open, num_close + 1)
+                dfs(curr, num_open, num_close + 1)
                 curr.pop()
-        
-        backtrack(["("], 1, 0)
-        return res
 
+        dfs(["("], 1, 0)
+        return res
