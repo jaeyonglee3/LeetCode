b11ad68a82e234c2b2fbb87b2269fc175fffb254
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-18 13:49:07 -0400

    Time: 64 ms (24.59%), Space: 21 MB (81.39%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,30 +7,12 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # key insight: the LCA is always the node that has p as part of one of its subtrees,
-        # and q as part of its other subtree.
-        # the LCA could also be either p or q themselves if no such node as described above exists
-
-        # assumptions + clarifications
-        # - all node values ARE in fact unique
-        # - p and q will be different nodes
-        # - p and q will exist in the BST
-
-        # intuition
-        # - we can run a DFS that follows one path down the tree. 
-        # Since we have a BST, we can easily check if the current
-        # node satisfies the key insight condition.
-
-        if p.val > root.val and q.val > root.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        elif p.val < root.val and q.val < root.val:
+        # find the node which is one of p or q is greater and the other is lesser
+        # in both cases, the node can be equal to in value as well
+        # since we allow a node to be a descendant of itself
+        if p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
         else:
             return root
-        
-        # time: O(h) where h is the height of the tree.
-        # Unlike a general tree traversal (which takes O(N) because it visits every node), 
-        # this algorithm only follows one path down the tree. Since the BST property allows us 
-        # to decide at each step whether to go left or right, we prune half the tree at each recursive call.
-        
-        # space: O(h) where h is the height of the tree, usually logn (balanced) or n worst case (skewed)
