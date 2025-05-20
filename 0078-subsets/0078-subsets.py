6d1f847cf3c7361e685ea562a8ba94252802af4b
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-20 19:29:15 -0400

    Time: 0 ms (100%), Space: 17.7 MB (90.51%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,9 +2,9 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        def dfs(i, curr):
+        def dfs(i, curr) -> None:
             if i > len(nums) - 1:
-                res.append(curr[:])
+                res.append(curr.copy())
                 return
             
             curr.append(nums[i])
