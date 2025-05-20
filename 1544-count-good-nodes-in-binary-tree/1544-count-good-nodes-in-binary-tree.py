7785b5f5a64f44689552173046e34761f89bf430
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-19 23:52:34 -0400

    Time: 125 ms (94.57%), Space: 32.3 MB (33.08%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -6,32 +6,20 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        res = 0
-        q = deque()
+        self.res = 0
 
-        # Track the maximum value (max_val) seen along the path from the root to the current node.
-        q.append((root, -math.inf))
-        
-        while q:
-            node, max_val = q.popleft()
-
-            if node.val >= max_val:
-                res += 1
+        def dfs(node, greatest) -> None:
+            if not node:
+                return
             
-            # when you append a left or right subtree to the queue, you always have the node
-            # itself as reference, so you can easily check the max value seen along the path
-            # from the root to current node.
-            if node.left:    
-                q.append((node.left, max(max_val, node.val)))
+            next_greatest = greatest
             
-            if node.right:
-                q.append((node.right, max(max_val, node.val)))
-                
-        return res
-
-        # time: O(n), we visit each node exactly once, , we perform O(1) 
-        # operations (checking the value, updating max_val, and adding children to the queue).
+            if node.val >= greatest:
+                self.res += 1
+                next_greatest = node.val
+            
+            dfs(node.left, next_greatest)
+            dfs(node.right, next_greatest)
         
-        # space: O(n)
-        # Queue holds at most one level at a time, which is O(N/2) = O(N) 
-        # in the worst case (for a full binary tree).
+        dfs(root, root.val)
+        return self.res
