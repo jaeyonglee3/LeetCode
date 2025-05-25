c1af783ea272ca93a16b0220ef7082155a53303e
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-24 21:27:05 -0400

    Time: 0 ms (100%), Space: 18.5 MB (39.22%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -7,19 +7,18 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         res = []
-        
-        def dfs(node, depth):
-            if not node: return
 
-            # Each list in res corresponds to a level in the tree.
-            # if len(res) == depth is true, we're at a new depth, so add a new list.
-            # if the len(res) exceeds depth, the list corresponding to that depth already exists.
+        def dfs(node, depth) -> None:
+            if not node:
+                return
+            
             if len(res) == depth:
                 res.append([])
             
             res[depth].append(node.val)
+            
             dfs(node.left, depth + 1)
             dfs(node.right, depth + 1)
-
+        
         dfs(root, 0)
         return res
