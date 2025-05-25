f256fdffea92f1a2381c9600a80f613f36a3d290
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-25 13:24:55 -0400

    Time: 0 ms (100%), Space: 19.1 MB (23.6%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,19 +6,21 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if not root: return False
+        if not root:
+            # since the tree is empty, there are no root-to-leaf paths. 
+            return False
 
         def dfs(node, curr_sum) -> bool:
             if not node:
+                # the parent node that made the recurisve call on this node was not a leaf
+                # thus we don't have a root-to-leaf path, so always return false
                 return False
             if not node.left and not node.right:
                 # we are at a leaf node
+                # return true if the current sum's value plus the leaf node's value adds up to target
                 return curr_sum + node.val == targetSum
             
             curr_sum += node.val
-            if dfs(node.left, curr_sum):
-                return True
-            
-            return dfs(node.right, curr_sum)
+            return dfs(node.left, curr_sum) or dfs(node.right, curr_sum)
             
         return dfs(root, 0)
