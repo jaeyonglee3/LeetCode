a37eadaddb5e2700b3ebef5341b027c0b880befd
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-27 18:11:10 -0400

    Time: 0 ms (100%), Space: 17.8 MB (60.93%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.py
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.py
@@ -6,16 +6,12 @@
 #         self.right = right
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node) -> bool:
-            # runs a dfs that visits every node,
-            # return true if every node has the same value as the val param
+        def isUnivalued(node) -> bool:
             if not node:
                 return True
             if node.val != root.val:
                 return False
             
-            # still efficient, as python will check left side first
-            # so dfs(node.right) runs only if dfs(node.left) returns True
-            return dfs(node.left) and dfs(node.right)
+            return isUnivalued(node.left) and isUnivalued(node.right)
         
-        return dfs(root)
+        return isUnivalued(root)
