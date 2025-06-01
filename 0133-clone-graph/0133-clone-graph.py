a7f9a75e0633924c7f764f87573634c44348882b
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-05-31 22:55:50 -0400

    Time: 46 ms (38.34%), Space: 18.1 MB (57.05%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,30 +1,19 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
 from typing import Optional
+
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        # with BFS
         if not node:
             return None
-        
+
+        visited = {node: Node(node.val)}
         q = collections.deque([node])
-        visited = {}
-        visited[node] = Node(node.val)
 
         while q:
             curr = q.popleft()
-            
-            for n in curr.neighbors:
-                if n not in visited:
-                    visited[n] = Node(n.val)
-                    q.append(n)
-                
-                visited[curr].neighbors.append(visited[n])
-        
+            for neighbor in curr.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val)
+                    q.append(neighbor)
+                visited[curr].neighbors.append(visited[neighbor])
+
         return visited[node]
