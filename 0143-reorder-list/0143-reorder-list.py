bca56a902832ae522f1538a53ae8cd2e78c258c5
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-01 23:41:09 -0400

    Time: 6 ms (25.4%), Space: 23.4 MB (12.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,26 +8,51 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # Use slow, fast, prev pointers
-        slow = head
-        fast = head.next
+        if head.next == None: return
+        # step 1: find the middle
+        # step 2: split the linked list into 2 halves (the middle node will be the starting node for the 2nd half)
+        # step 3: reverse the 2nd half
+        # step 4: combine the two halves together and return the head
 
-        while slow:
-            prev = None
-
-            while fast and fast.next:
-                prev = fast
+        # step 1
+        middle, fast = head, head
+        while fast:
+            fast = fast.next
+            if fast:
                 fast = fast.next
-            
-            if prev:
-                prev.next = None
-                slow_next_old = slow.next
-                slow.next = fast
-                
-                fast.next = slow_next_old
-                slow = slow_next_old
-
-                # eventually fast becomes none when slow and prev are next to each other
-                fast = slow.next  
             else:
-                return
+                # middle now points to the correct middle
+                break
+            middle = middle.next
+        
+        # step 2
+        # now middle is the starting node for the 2nd half
+        curr = head
+        while curr:
+            if curr.next == middle:
+                curr.next = None
+                break
+            curr = curr.next
+
+        # step 3
+        curr, prev = middle, None
+        while curr:
+            old_next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = old_next
+        
+        # step 4
+        # now 'prev' is the head to the reversed 2nd half
+        curr1, curr2 = head, prev
+        while curr1 and curr2:
+            tmp1 = curr1.next
+            tmp2 = curr2.next
+
+            curr1.next = curr2
+            if tmp1 is None:
+                break
+            curr2.next = tmp1
+
+            curr1 = tmp1
+            curr2 = tmp2
