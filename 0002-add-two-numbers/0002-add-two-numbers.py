6c2ba481bc17794d98586f8845c796a7b7ac3204
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-03 00:12:23 -0400

    Time: 6 ms (37.24%), Space: 18.1 MB (14.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,39 +6,37 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         curr1, curr2 = l1, l2
-        dummy_head = dummy_curr = ListNode()
-        is_carry = False
+        result = curr = ListNode()
+        carry = 0
 
-        while curr1 or curr2:
-            curr1_val = 0 if not curr1 else curr1.val
-            curr2_val = 0 if not curr2 else curr2.val
+        while curr1 and curr2:
+            total = curr1.val + curr2.val + carry
+            carry = 0 if total < 10 else 1
 
-            result = (curr1_val + curr2_val)
-            if is_carry:
-                is_carry = False
-                result += 1
-            
-            if result > 9:
-                result = result % 10
-                is_carry = True
-    
-            new_node = ListNode(result)
-            dummy_curr.next = new_node
+            if total == 10:
+                carry = 1
 
-            curr1 = curr1.next if curr1 else None
-            curr2 = curr2.next if curr2 else None
-            dummy_curr = dummy_curr.next
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+
+            curr1 = curr1.next
+            curr2 = curr2.next
         
-        if is_carry:
-            dummy_curr.next = ListNode(1)
-
-        return dummy_head.next
-
-# The time complexity of this solution is O(max(N, M)), 
-# where N and M are the lengths of l1 and l2. 
-# This is because we traverse both linked lists once, 
-# processing each node in a single pass.
-
-# The space complexity is also O(max(N, M)) since we 
-# create a new linked list to store the sum, 
-# with at most one extra node for a carry at the end.
+        while curr1:
+            total = curr1.val + carry
+            carry = 0 if total < 10 else 1
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+            curr1 = curr1.next
+        
+        while curr2:
+            total = curr2.val + carry
+            carry = 0 if total < 10 else 1
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+            curr2 = curr2.next
+        
+        if carry: 
+            curr.next = ListNode(carry)
+        
+        return result.next
