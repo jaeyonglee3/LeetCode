f8eabadfaa9693e0052332a92f14af2ae0f7fdf4
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-09 19:03:25 -0400

    Time: 11 ms (83.34%), Space: 20.4 MB (95.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        strs_map = collections.defaultdict(list)
+        # use a hashmap where the keys are the words sorted alphabetically
+        # and the values are all the anagrams of that word.
+        anagrams = {}
 
         for word in strs:
-            sorted_word = "".join(sorted(word))
-            strs_map[sorted_word].append(word)
-        
-        return list(strs_map.values())
+            sorted_word = ''.join(sorted(word))
+
+            if sorted_word in anagrams:
+                anagrams[sorted_word].append(word)
+            else:
+                anagrams[sorted_word] = [word]
+            
+        # need to return a nested list of all the anagrams grouped together
+        return list(anagrams.values())
