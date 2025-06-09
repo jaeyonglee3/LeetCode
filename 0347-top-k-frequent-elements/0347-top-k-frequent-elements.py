b9d86d5aa7b6ecd3d2fa13f5f017c327508eda1c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-09 19:28:37 -0400

    Time: 4 ms (69.67%), Space: 21 MB (89.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,9 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_map = collections.defaultdict(int)
+        freq = collections.defaultdict(int)
 
         for n in nums:
-            freq_map[n] += 1
+            freq[n] += 1
         
-        freq_elements = sorted(freq_map.keys(), key=lambda val: freq_map[val], reverse=True)
-        return freq_elements[:k]
+        # freq.keys() - This gives you an iterable of all the keys in freq.
+        # The sorted() function takes an iterable and returns a new sorted list.
+        # For each key val in freq.keys(), this lambda returns freq_map[val], i.e. its frequency.
+        # reverse the sort to sort in descending order (highest first)
+        freq_elements = sorted(freq.keys(), key=lambda val : freq[val], reverse=True)
+        return freq_elements[ : k]
