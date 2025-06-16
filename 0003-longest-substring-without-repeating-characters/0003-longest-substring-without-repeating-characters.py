26fd9b8a0b35635259874cf0f50013ea46e8e24f
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-16 14:32:34 -0400

    Time: 17 ms (63.94%), Space: 17.8 MB (52.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,8 +3,8 @@ class Solution:
         # want to find the longest window within s in which there are no repeating characters
         # the window's start and end will be marked with l and r pointers
         
-        # if the right is pointing to a character that is already inside the window,
-        # move the left pointer to the index of the repeated character + 1. R ptr still increments by 1 as usual.
+        # Key Insight: if the right is pointing to a character that has already been seen within the window,
+        # move the left pointer to the index of the repeated character + 1. Right ptr still increments by 1 as usual.
         # Take 'azzbcbb' as an example. When right is pointing to the 2nd last b, left needs to move up all the way to index 4
         
         # Edge case - an empty string will always return 0
