f0a28edeb203b4aec28d89c2427e1e8bba67bb6f
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-16 13:43:59 -0400

    Time: 13 ms (86.96%), Space: 18 MB (34.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,7 +2,14 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # want to find the longest window within s in which there are no repeating characters
         # the window's start and end will be marked with l and r pointers
-        if len(s) == 0: return 0
+        
+        # if the right is pointing to a character that is already inside the window,
+        # move the left pointer to the index of the repeated character + 1.
+        # right ptr still increments by 1 as usual.
+        
+        # Edge case - an empty string will always return 0
+        if len(s) == 0: 
+            return 0
         
         res = 1
         l, r = 0, 1
