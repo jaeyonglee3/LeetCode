7da79a6039c9d119c5e26e20e2e0a2139859ee94
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-16 14:37:04 -0400

    Time: 263 ms (38.05%), Space: 20 MB (95.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # iterate over the grid. each time a 1 is encountered, call a dfs() helper
-        # on it and pass its r, c coordinates. increment result by 1.
-        # dfs() helper responsible for turning that cell and all connected 1 cells
-        # into 0s to mark them as "visited," so that land cells that are part of
-        # a singular island are certainly only counted once
-        ROWS, COLS = len(grid), len(grid[0])
-        DIRS = [(1, 0), (-1, 0), (0, -1), (0, 1)]
         res = 0
+        ROWS, COLS = len(grid), len(grid[0])
 
         def dfs(r, c):
             if min(r, c) < 0 or r == ROWS or c == COLS or grid[r][c] == "0":
                 return
             
             grid[r][c] = "0"
-            for dr, dc in DIRS:
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 dfs(r + dr, c + dc)
-        
+
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    dfs(r, c)
+                    # Use some dfs technique to change all connected 1s as 0s
+                    # this effectively marks all the connected 1 cells as "visited"
+                    # this ensures we count each island only once
                     res += 1
+                    dfs(r, c)
         
         return res
