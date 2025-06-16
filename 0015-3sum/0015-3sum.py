010fc704b2a1366c1a60cf6e29502257e41bcbfc
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-16 01:33:07 -0400

    Time: 475 ms (83.06%), Space: 20.7 MB (37.05%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,13 +1,11 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-
         nums.sort()
 
         # so we need triplets of numbers a, b, and c.
         # lets iterate over nums, fix an a, and find b and c
         # such that b + c = -a, meaning a + b + c = 0
-
         for i_a, a in enumerate(nums):
             if i_a > 0 and a == nums[i_a - 1]:
                 continue
@@ -20,7 +18,9 @@ class Solution:
                 
                 if curr == target:
                     res.append([a, nums[r], nums[l]])
-                    
+                    # We only skip over duplicates of nums[l] and nums[r] after we’ve used them in a result, to avoid repeating the same triplet. 
+                    # But if there’s no match, we’re free to explore all options — including duplicates — because they might eventually 
+                    # contribute to a valid triplet with a different combination.
                     while l < len(nums) - 1 and nums[l] == nums[l + 1]:
                         l += 1
                     while r > 0 and nums[r] == nums[r - 1]:
