2854582328a35c13d7c8719f8e3cc56ee5a05982
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-18 01:54:09 -0400

    Time: 95 ms (92.99%), Space: 17.9 MB (86.59%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,43 +1,26 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # # Variable sized sliding window technique
-        # left = right = 0
-        # count = {}
-        # res = 0
+        # use a sliding window that starts with the first char
+        # keep expanding the sliding window. Each time, ensure that
+        # (the number of characters) - (count of most freq char in the window) <= k
+        # that difference tells you how many characters need to be replaced in that curr window
+        # if we exceed k, shift l to where r is, and r += 1. shift window over completely
 
-        # while right <= len(s) - 1:
-        #     # At each window, we need to update our hashmap w/ character counts
-        #     substring = s[left : right + 1]
-        #     unique_chars = set(substring)
+        res = 1
+        l = 0
+        window_freq = {s[0] : 1}
+        most_freq = (s[0], 1)
 
-        #     for c in unique_chars:
-        #         count[c] = substring.count(c)
+        for r in range(1, len(s)):
+            window_freq[s[r]] = window_freq.get(s[r], 0) + 1
             
-        #     # Subtract most freq char count from length of substring 
-        #     most_freq = max(count.values())
-        #     total_replacements = (right - left + 1) - most_freq
+            if window_freq[s[r]] > most_freq[1]:
+                most_freq = (s[r], window_freq[s[r]])
 
-        #     if total_replacements <= k:
-        #         res = max(res, len(substring))
-        #     else:
-        #         left += 1
+            if (r - l + 1) - most_freq[1] <= k:
+                res = max(res, r - l + 1)
+            else:
+                window_freq[s[l]] -= 1
+                l += 1
 
-        #     right += 1
-
-        # return res
-
-        count = {}
-        res = 0
-
-        left = 0
-        for right in range(len(s)):
-            count[s[right]] = 1 + count.get(s[right], 0)
-
-            if (right - left + 1) - max(count.values()) > k:
-                count[s[left]] -= 1
-                left += 1
-
-            res = max(res, right - left + 1)
         return res
-
-        
