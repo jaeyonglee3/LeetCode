04189017fb52ff2181e5878092f4cf2c68a5ae4d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-19 01:01:24 -0400

    Time: 19 ms (66.84%), Space: 17.9 MB (62.23%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,35 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # Trivial edge case
-        # s2 cannot have a permutation of s1 if s1 is longer
-        if len(s1) > len(s2): return False
-        
-        # Build frequency map for s1 (will not change)
-        freq_s1 = Counter(s1)
+        if len(s1) > len(s2) or len(s2) == 0:
+            return False
 
-        # Build frequency map for s2 (changes as window slides over s2)
-        freq_s2 = Counter(s2[:len(s1)])
+        s1_freq = {}
+        for c in s1:
+            s1_freq[c] = s1_freq.get(c, 0) + 1
+
+        l, r = 0, 1
+        substring_freq = {s2[0] : 1}
+        while r < len(s1):
+            substring_freq[s2[r]] = substring_freq.get(s2[r], 0) + 1
+            r += 1
         
-        if freq_s1 == freq_s2: return True
-        
-        # Use fixed sliding window technique to slide over s2
-        # left pointer always points to the char to be REMOVED from the window
-        # right pointer points to the new char we are adding to our window
-        l, r = 0, len(s1)
-        while r < len(s2):
-            freq_s2[s2[r]] += 1  # Add new char to window
-            freq_s2[s2[l]] -= 1  # Remove old char from window
+        r = len(s1) - 1
+        while r < len(s2) and substring_freq != s1_freq:
+            substring_freq[s2[l]] -= 1
+            if substring_freq[s2[l]] == 0:
+                del substring_freq[s2[l]]
             
-            if freq_s2[s2[l]] == 0:
-                del freq_s2[s2[l]]  # Remove entry from dict if count becomes 0
-            
-            # Slide the window along s2
             l += 1
             r += 1
 
-            if freq_s1 == freq_s2: return True
-        
-        return False
+            if r > len(s2) - 1:
+                return False
 
-        # time: O(|s2|) since we slide our window over s2 and frequency map updates are O(1)
-        # space: O(1) constant b/c freq maps can have at most 26 entries
+            substring_freq[s2[r]] = substring_freq.get(s2[r], 0) + 1
+        
+        return substring_freq == s1_freq
+
+        
+
