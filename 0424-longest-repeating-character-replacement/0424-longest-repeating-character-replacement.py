5b803d726ddcaa869d35505bcf13fead954f94e5
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-19 00:46:43 -0400

    Time: 94 ms (93.09%), Space: 18 MB (48.66%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,26 +1,32 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # use a sliding window that starts with the first char
-        # keep expanding the sliding window. Each time, ensure that
-        # (the number of characters) - (count of most freq char in the window) <= k
-        # that difference tells you how many characters need to be replaced in that curr window
-        # if we exceed k, shift l to where r is, and r += 1. shift window over completely
+        # key insight 1: use a sliding window to go over substrings in s
+        # and find the longest one that can be made of the same letter with k operations
 
+        # key insight 2: to calculate the number of operations needed in a window,
+        # use len(current window) minus number of the most frequent character in that window.
+
+        if not s:
+            return 0
+        
         res = 1
-        l = 0
-        window_freq = {s[0] : 1}
+        char_freq = {s[0] : 1}
         most_freq = (s[0], 1)
+        l = 0
 
         for r in range(1, len(s)):
-            window_freq[s[r]] = window_freq.get(s[r], 0) + 1
+            char_freq[s[r]] = char_freq.get(s[r], 0) + 1
+
+            if char_freq[s[r]] > most_freq[1]:
+                most_freq = (s[r], char_freq[s[r]])
             
-            if window_freq[s[r]] > most_freq[1]:
-                most_freq = (s[r], window_freq[s[r]])
+            # calculate the number of operations needed in window
+            num_ops = (r - l + 1) - most_freq[1]
 
-            if (r - l + 1) - most_freq[1] <= k:
-                res = max(res, r - l + 1)
+            if num_ops <= k:
+                res = max(res, (r - l + 1))
             else:
-                window_freq[s[l]] -= 1
+                char_freq[s[l]] -= 1
                 l += 1
-
+        
         return res
