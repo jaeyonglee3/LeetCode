dbce9d450fe44403858e9ad9b2558004b879d69c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-24 21:57:53 -0400

    Time: 106 ms (21.67%), Space: 27.2 MB (95.43%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,21 +1,17 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        # Attempt 1: TLE, list slicing takes too long
-        # l_pointer = 0
-        # max_avg = -math.inf
+        l, r = 0, k - 1
+        max_sum = curr_sum = sum(nums[0 : k])
 
-        # for r_pointer in range(k - 1, len(nums)):
-        #     # avg = sum(nums[l_pointer : r_pointer + 1]) / k
-        #     avg = prev_avg - num[l_pointer - 1]
-        #     max_avg = max(max_avg, avg)
-        #     l_pointer += 1
+        while r < len(nums):
+            curr_sum -= nums[l]
+
+            # slide the window over
+            l += 1
+            r += 1
+
+            if r < len(nums):
+                curr_sum += nums[r]
+                max_sum = max(max_sum, curr_sum)
         
-        # return max_avg
-
-        avg = max_avg = sum(nums[:k]) / k
-
-        for i in range(k, len(nums)):
-            avg += (nums[i] - nums[i - k]) / k
-            max_avg = max(avg, max_avg)
-        
-        return max_avg
+        return max_sum / k
