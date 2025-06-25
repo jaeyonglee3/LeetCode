05077dbde899edbbfbf1ea050848f151cf1ce3e5
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-24 23:14:26 -0400

    Time: 151 ms (91.1%), Space: 28.7 MB (91.46%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.py
@@ -1,29 +1,27 @@
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        # Hashset solution: construct two hashsets, each containing the possible
-        # prefixes of each number from both arr1 and arr2. Then, do a set intersection
-        # and return the max length from the prefixes that result from it.
-        arr1 = [str(n) for n in arr1]
-        arr2 = [str(n) for n in arr2]
-        
-        set1 = set()
-        set2 = set()
+        # NeetCode video solution - most efficient but requires some tricks
+        prefix_set = set()
+        res = 0
 
         for n in arr1:
-            curr_prefix = []
-            for digit in n:
-                curr_prefix.append(digit)
-                set1.add(''.join(curr_prefix))
+            while n and n not in prefix_set:
+                prefix_set.add(n)
+                # use // 10 to remove the rightmost digit
+                # we start with each numbers LARGEST possible prefix because
+                # it is likely that its smaller prefixes are already in the prefix set.
+                # so do n // 10 and keep going until n is either 0 or n is already in the set.
+                n = n // 10
         
         for n in arr2:
-            curr_prefix = []
-            for digit in n:
-                curr_prefix.append(digit)
-                set2.add(''.join(curr_prefix))
+            while n and n not in prefix_set:
+                # now, we want to update the result only if we find a prefix in arr2 that is in prefix_set.
+                # again start with the biggest prefix n can be, and // 10 to remove rightmost digit each time.
+                # if this loop stops before n == 0, that means we've found a prefix that some num in arr1
+                # and some num in arr2 share. So update the result only if n does not become 0.
+                n = n // 10
+            
+            if n != 0:
+                res = max(res, len(str(n)))
         
-        common_prefixes = set1.intersection(set2)
-
-        if len(common_prefixes) == 0:
-            return 0
-        else:
-            return max(len(prefix) for prefix in common_prefixes)
+        return res
