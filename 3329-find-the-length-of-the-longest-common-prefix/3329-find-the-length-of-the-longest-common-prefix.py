98552d79294c794735dc4deafd19ff0b392a9c03
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-06-24 22:44:56 -0400

    Time: 503 ms (23.73%), Space: 43.1 MB (5.22%) - LeetHub

---
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        # Hashset solution: construct two hashsets, each containing the possible
+        # prefixes of each number from both arr1 and arr2. Then, do a set intersection
+        # and return the size of the set that results from it.
+        arr1 = [str(n) for n in arr1]
+        arr2 = [str(n) for n in arr2]
+        
+        set1 = set()
+        set2 = set()
+
+        for n in arr1:
+            curr_prefix = []
+            for digit in n:
+                curr_prefix.append(digit)
+                set1.add(''.join(curr_prefix))
+        
+        for n in arr2:
+            curr_prefix = []
+            for digit in n:
+                curr_prefix.append(digit)
+                set2.add(''.join(curr_prefix))
+        
+        common_prefixes = set1.intersection(set2)
+
+        if len(common_prefixes) == 0:
+            return 0
+        else:
+            return max(len(prefix) for prefix in common_prefixes)
