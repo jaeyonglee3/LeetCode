f21513a5ef4ad7f9ef68bf2af17e58e2e24f4c82
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-01 01:33:34 -0400

    Time: 113 ms (51.63%), Space: 33.2 MB (32.55%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,15 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # monotonically decreasing stack problem!
-        res = [0] * len(temperatures)
-        stack = []  # contains (index, temp) pairs
+        # monotonically increasing stack application
+        answer = [0] * len(temperatures)
+        stack = []  # stores temp, index pairs
 
-        for i, temp in enumerate(temperatures):
-            while stack and temp > stack[-1][1]:
-                removed_i = stack.pop()[0]
-                res[removed_i] = i - removed_i
+        for cur_i, cur_temp in enumerate(temperatures):
+            # before you add the tuple, check if the cur_temp is greater
+            # than the temp at the top of the stack because then, we've found a warmer temp
+            # and we need to update the answer array
+            while stack and cur_temp > stack[-1][0]:
+                removed_i = stack.pop()[1]
+                answer[removed_i] = cur_i - removed_i
             
-            stack.append((i, temp))
+            stack.append((cur_temp, cur_i))
         
-        return res
-
+        return answer
