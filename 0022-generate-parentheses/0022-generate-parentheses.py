5d1c31a622d89138f97c6d5aac0e4f338f6a7ab6
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-06 00:26:13 -0400

    Time: 3 ms (29.8%), Space: 17.9 MB (69.2%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,24 +1,30 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # while forming combinations: 
-        # of closing parentheses can never exceed the number of opening.
-        # number of open parentheses can never exceed n.
+        # backtracking solution
         res = []
 
-        def dfs(curr, num_open, num_close):
+        def dfs(curr_str, num_open, num_close):
+            # this helper will run the BT algorithm and populate the result
+            # left branch: always add another open parentheses
+            # righgt branch: always add another closing parentheses
+            # left branch should only occur if num_open < n
+            # right branch should only occur if num_open > num_close
+
+            # base case: there are exactly n pairs of open and close parentheses
             if num_open == num_close == n:
-                res.append("".join(curr[:]))
+                res.append(''.join(curr_str.copy()))
                 return
             
+            curr_str.append('(')
             if num_open < n:
-                curr.append("(")
-                dfs(curr, num_open + 1, num_close)
-                curr.pop()
+                dfs(curr_str, num_open + 1, num_close)
             
-            if num_close < num_open:
-                curr.append(")")
-                dfs(curr, num_open, num_close + 1)
-                curr.pop()
+            curr_str.pop()
 
-        dfs(["("], 1, 0)
+            if num_open > num_close:
+                curr_str.append(')')
+                dfs(curr_str, num_open, num_close + 1)
+                curr_str.pop()
+            
+        dfs(['('], 1, 0)
         return res
