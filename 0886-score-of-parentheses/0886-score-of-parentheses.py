37a1a636727a056a98f071519556e7c19a075f01
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-06 18:09:35 -0400

    Time: 0 ms (100%), Space: 17.9 MB (26.98%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.py
+++ 0886-score-of-parentheses/0886-score-of-parentheses.py
@@ -6,23 +6,23 @@ class Solution:
         for par in s:
             if par == '(':
                 stack.append(par)
-            else:
-                to_multiply = None
-                while stack and type(stack[-1]) == int:
-                    num = stack.pop()
-                    
-                    if to_multiply:
-                        to_multiply += num
-                    else:
-                        to_multiply = num
-                
-                if stack:
-                    stack.pop()  # remove the opening bracket that has just been closed
+                continue
+
+            # here, par == ')'
+            to_multiply = None
+            while stack and type(stack[-1]) == int:
+                num = stack.pop()
                 
                 if to_multiply:
-                    num = to_multiply * 2
-                    stack.append(num)
+                    to_multiply += num
                 else:
-                    stack.append(1)
+                    to_multiply = num
+            
+            if stack: stack.pop()  # remove the opening bracket that has just been closed
+            
+            if to_multiply:
+                stack.append(to_multiply * 2)
+            else:
+                stack.append(1)
 
         return sum(stack)
