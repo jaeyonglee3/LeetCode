368960c2d1811b62a42d74bf546d9645f3c0346f
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-06 19:03:17 -0400

    Time: 71 ms (16.63%), Space: 18 MB (35.97%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,23 +1,45 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def isPalindrome(l, r):
-            while r >= l:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
+        # a palindrome is a string that reads the same forwards and backwards
+
+        # s = "abbca"
+        # left = b
+        # right = c
+
+        # Idea
+        # start with two pointers left and right at the start and end of the string
+        # move them inwards as long as they point to the same character
+        # If we encounter s[left] != s[right]
+            # we need to examine whether s[left] or s[right] should be removed
+            # substring 1 excludes s[right] = s[left : right]
+            # substring 2 excludes s[left] = s[left + 1 : right + 1]
+            # if either or both of these two are palindromes, return TRUE
+            # if neighter are palindromes, return FALSE
         
         l, r = 0, len(s) - 1
-        while r >= l:
-            if s[r] != s[l]:
-                # check if the string can still be a palindrome after
-                # remove either s[r] or s[l]
-                return isPalindrome(l + 1, r) or isPalindrome(l, r - 1)
+
+        while r > l:
+            if s[l] != s[r]:
+                # formulate substrings 1 and 2, check if they're palindromes
+                substring1 = s[l : r]
+                if self.isPalindrome(substring1):
+                    return True
+                
+                substring2 = s[l + 1 : r + 1]
+                return self.isPalindrome(substring2)
             
             l += 1
             r -= 1
         
         return True
+    
+    def isPalindrome(self, s):
+        l, r = 0, len(s) - 1
 
-        # O(N) time, O(1) space
+        while r > l:
+            if s[l] != s[r]: return False
+            l += 1
+            r -= 1
+        
+        return True
+
