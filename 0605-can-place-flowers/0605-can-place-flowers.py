9f3a3521cb596228bc53a831144e3770eaeacdc1
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-06 18:35:55 -0400

    Time: 7 ms (75.42%), Space: 18.4 MB (14.3%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,22 +1,32 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        res = 0
+        # paraphrase
+        # 0 == empty
+        # 1 == non-empty
+        # Flowers cannot be planted next to one each other
+        # So, in order to plant a flower, we need to find an index i within flowerbed where
+            # flowerbed[i] == 0
+            # flowerbed[i - 1] == 0 or out of range
+            # flowerbed[i + 1] == 0 or out of range
         
+        # sol'n approach
+        # do a linear scan over flowerbed. each time we find an index i that meets the criteria above,
+        # increment a counter, and return whether the counter == n
+        # We can early return right when counter == n
+
+        # edge case: when not adding any more flowers
+        if n == 0: return True
+
+        # this counts the number of additional flowers we can add
+        num_flowers = 0
+
         for i in range(len(flowerbed)):
-            if flowerbed[i] != 1:
-                if i == 0 and (len(flowerbed) == 1 or flowerbed[i + 1] == 0):
-                    res += 1  
-                    flowerbed[i] = 1
-                elif i == (len(flowerbed) - 1) and flowerbed[i - 1] == 0:
-                    res += 1 
-                    flowerbed[i] = 1
-                elif (not (i == 0 or i == len(flowerbed) - 1) and 
-                    flowerbed[i + 1] != 1 and flowerbed[i - 1] != 1):
-                    res += 1
-                    flowerbed[i] = 1
-            else:
-                pass
+            if flowerbed[i] == 0 and (i - 1 < 0 or flowerbed[i - 1] == 0) and (i + 1 >= len(flowerbed) or flowerbed[i + 1] == 0):
+                flowerbed[i] = 1
+                num_flowers += 1
+            
+            if num_flowers == n:
+                return True
+            
+        return num_flowers == n
 
-        return res >= n
-
-        
