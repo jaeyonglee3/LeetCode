15eb7ff13674d05282006a434f54960a6520fc38
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-06 23:25:35 -0400

    Time: 2 ms (85.65%), Space: 18.5 MB (59.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,21 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # paraphrase
+        # given a sorted array called numbers, 
+        # return a list [a + 1, b + 1] where numbers[a] + numbers[b] == target
+        # every input has exactly one solution (we're guanrateed to have a solution)
+        
+        # dry runs
+        # [2,7,11,15]
+        # 2 + 7 is 9 so return [1, 2]
+        
+        # approach
+        # have two pointers l, r at the ends of numbers. let sum = nums[l] + num[r]
+            # if sum > target: r -= 1
+            # if sum < target: l += 1
+            # if sum is target: return [l + 1, r + 1]
+        
+        # implement
         l, r = 0, len(numbers) - 1
 
         while r > l:
