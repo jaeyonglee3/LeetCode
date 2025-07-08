f5743b35e42d4a6310e4171fda812743645f0933
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-07 23:35:40 -0400

    Time: 0 ms (100%), Space: 17.7 MB (62.9%) - LeetHub

--- 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.py
+++ 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.py
@@ -9,12 +9,12 @@ class Solution:
         for c in s:
             if c in target:
                 s_freq[c] = s_freq.get(c, 0) + 1
-        
-        if set(s_freq.keys()) != set(target):
-            return 0
 
         counts = []
         for c in t_freq:
+            if c not in s_freq:
+                return 0
+
             counts.append(s_freq[c] // t_freq[c])
         
         return min(counts)
