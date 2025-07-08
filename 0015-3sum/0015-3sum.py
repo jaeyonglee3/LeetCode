5684ca825ca025407921b0f0506223eeae1284b5
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-07 22:25:32 -0400

    Time: 811 ms (24.43%), Space: 20.7 MB (36.65%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,54 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # paraphrase
+        # given an array nums, return a list of lists, containing all groups of 3
+        # numbers from the array that sum to 0
+        # no duplicates in groups of 3, and same element can't be used more than once in a group
+
+        # dry runs (examples)
+        # nums = [-1,0,1,2,-1,-4]
+        # sort_nums = [-4, 2, 2, 2, 2, 5, 5, 5]
+        # result = [[-1, 0, -1], [-1, -1, 2]]
+        # nums[i] = -1
+        # nums[j] = -1
+        # nums[k] = 2
+
+        # approach
+        # fix the nums[i] number first
+        # then, search the rest of the array for two numbers that add up to the negation of nums[i]
+        # we can do this using two pointers, 
+        # moving them depending on if the sum at the moment if too large or too little
+        # add the list of [nums[i], nums[j], nums[k]] to the result
+
+        # implement
+        nums.sort()  # O(n log n) operation
         res = []
-        nums.sort()
 
-        # so we need triplets of numbers a, b, and c.
-        # lets iterate over nums, fix an a, and find b and c
-        # such that b + c = -a, meaning a + b + c = 0
-        for i_a, a in enumerate(nums):
-            if i_a > 0 and a == nums[i_a - 1]:
+        for i, num_i in enumerate(nums):
+            if i > 0 and nums[i - 1] == num_i:
                 continue
-
-            target = -a
-            l, r = i_a + 1, len(nums) - 1
+                
+            target = -num_i
+            l, r = i + 1, len(nums) - 1
 
             while r > l:
-                curr = nums[l] + nums[r]
-                
-                if curr == target:
-                    res.append([a, nums[r], nums[l]])
-                    # We only skip over duplicates of nums[l] and nums[r] after we’ve used them in a result, to avoid repeating the same triplet. 
-                    # But if there’s no match, we’re free to explore all options — including duplicates — because they might eventually 
-                    # contribute to a valid triplet with a different combination.
-                    while l < len(nums) - 1 and nums[l] == nums[l + 1]:
-                        l += 1
-                    while r > 0 and nums[r] == nums[r - 1]:
-                        r -= 1
+                curr_sum = nums[l] + nums[r]
+
+                if curr_sum == target:
+                    res.append([num_i, nums[l], nums[r]])
                     r -= 1
                     l += 1
-                elif curr > target:
+                    while r > 0 and nums[r] == nums[r + 1]:
+                        r-= 1
+                    while l < len(nums) and nums[l] == nums[l - 1]:
+                        l += 1
+                elif curr_sum > target:
                     r -= 1
+                    while r > 0 and nums[r] == nums[r + 1]:
+                        r-= 1
                 else:
                     l += 1
+                    while l < len(nums) and nums[l] == nums[l - 1]:
+                        l += 1
         
         return res
