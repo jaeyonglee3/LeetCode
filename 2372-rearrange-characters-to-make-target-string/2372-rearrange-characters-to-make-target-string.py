067cff1fd7443646c896ad95033dbf6c8db8948f
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-07 23:46:48 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.py
+++ 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.py
@@ -11,20 +11,16 @@ class Solution:
             if c in target:
                 s_freq[c] = s_freq.get(c, 0) + 1
 
-        # counts stores 1 number for each character in target
-        # this number equals the number of times we can form the target string
-        # based on that char's frequency in s
-        counts = []
-
+        res = 0
         for c in t_freq:
             if c not in s_freq:
                 return 0
 
             # this floor division tells us how many of the target we can create
             # using the number of that char that was found in s
-            counts.append(s_freq[c] // t_freq[c])
+            res = min(res, s_freq[c] // t_freq[c])
         
         # this min value is the result because the limiting factor is the 
         # character we have the *least* of relative to what's needed in target
-        return min(counts)
+        return res
         
