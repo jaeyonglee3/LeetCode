fe078c093bd60afcaeea62cd3f1909907b7b2bde
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-08 01:14:08 -0400

    Time: 0 ms (100%), Space: 18.4 MB (25.48%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,18 +1,37 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        l, r = 0, ROWS * COLS - 1
+        # first find the inner list that can contain target
+        # do this with binary search
+        # if none is found, return false early
+        l, r = 0, len(matrix) - 1
+        row = None
 
         while r >= l:
-            mid = (l + r) // 2
-            row, col = mid // COLS, mid % COLS
-            curr_val = matrix[row][col]
+            mid = (r + l) // 2
+            curr_row = matrix[mid]
 
-            if curr_val == target:
+            if target >= curr_row[0] and target <= curr_row[-1]:
+                row = curr_row
+                break
+            elif target > curr_row[-1]:
+                l = mid + 1
+            elif target < curr_row[0]:
+                r = mid - 1
+        
+        if row == None:
+            return False
+        
+        l, r = 0, len(matrix[0]) - 1
+
+        while r >= l:
+            mid = (r + l) // 2
+
+            if row[mid] == target:
                 return True
-            elif curr_val > target:
+            elif row[mid] > target:
                 r = mid - 1
             else:
+                # row[mid] < target
                 l = mid + 1
         
         return False
