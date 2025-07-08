c73291830b8e963bf23e6c9f1d6846bebbcc6ac5
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-07 23:12:47 -0400

    Time: 99 ms (51.39%), Space: 19.4 MB (67.37%) - LeetHub

--- 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.py
+++ 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.py
@@ -5,23 +5,15 @@ class Solution:
             # it can have at most maxLetters
             # its length must be between [minSize, maxSize]
         # maxSize is at most 26
-
-        # Input: s = "aababcaab"
-        
-        diff = maxSize - minSize
         l, r = 0, minSize - 1
         freq = {}
         res = 0
 
         while r < len(s):
-            for end_i in range(r, r + diff + 1):
-                if end_i >= len(s):
-                    break
-                
-                substring = s[l : end_i + 1]
-                if len(set(substring)) <= maxLetters:
-                    freq[substring] = freq.get(substring, 0) + 1
-                    res = max(res, freq[substring])
+            substring = s[l : r + 1]
+            if len(set(substring)) <= maxLetters:
+                freq[substring] = freq.get(substring, 0) + 1
+                res = max(res, freq[substring])
                 
             l += 1
             r += 1
