59d91e9ab560396bfe87b6b88e7d8d624a0a090d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-08 19:44:00 -0400

    Time: 15 ms (78.37%), Space: 18.4 MB (95.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,12 +2,13 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         # Greedy approach
         # each num in nums represents the max jump length from that index
-        
-        max_reach = 0
-        for i in range(len(nums)):
-            if i > max_reach:
-                # If you ever reach an index that’s beyond your current maximum reach, then you're stuck
-                return False
-            max_reach = max(max_reach, i + nums[i])
-        return True
+        goal = len(nums) - 1
 
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                # then, we don't have to get to goal anymore to reach the end
+                # this means we just need to get to i
+                # so i becomes our new goal
+                goal = i
+        
+        return goal == 0
