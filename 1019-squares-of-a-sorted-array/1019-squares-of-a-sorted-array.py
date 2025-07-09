511b1c9db20cbffcbf0a4813efe29c632ddbb5a7
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-09 19:13:17 -0400

    Time: 8 ms (67.28%), Space: 19.7 MB (23.16%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -1,7 +1,26 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        for i, num in enumerate(nums):
-            nums[i] = num * num
+        # the largest squares produced will be the ones furthest from 0
+        # either at the far left (negative) or far right (positive)
+        # if the square of the number at l ptr is greater than the one at r ptr, swap them
+        # only increment the left pointer after a swap. if no swap, keep it there
+        # right ptr always moves inwards until it meets the left ptr
+
+        res = []
+        l, r = 0, len(nums) - 1
+        while r >= l:
+            sqr_r, sqr_l = nums[r] ** 2, nums[l] ** 2
+
+            if sqr_r > sqr_l:
+                res.append(sqr_r)
+                r -= 1
+            elif sqr_r < sqr_l:
+                res.append(sqr_l)
+                l += 1
+            else:
+                res.append(sqr_l)
+                if l != r: res.append(sqr_r)
+                r -= 1
+                l += 1
         
-        nums.sort()
-        return nums
+        return list(reversed(res))
