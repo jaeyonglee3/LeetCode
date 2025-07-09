96079e823575826d0efcdc1b8b24e92cb8cee421
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-09 00:19:22 -0400

    Time: 1 ms (87.44%), Space: 18.4 MB (99.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,21 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Attempt 1 (correct), unnecessary to start l and r at index of first zero
-        # first_zero = nums.index(0) if 0 in nums else -1
-        # if first_zero == -1:
-        #     return
-
-        # l, r = first_zero, first_zero
-
-        # while r + 1 < len(nums):
-        #     r += 1
-        #     if nums[r] != 0:
-        #         nums[l], nums[r] = nums[r], nums[l]
-        #         l += 1
-        
         l = 0
+
         for r in range(len(nums)):
-            if nums[r]:
+            if nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
