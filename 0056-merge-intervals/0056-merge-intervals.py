f99474bcb10c5c3c26d07c3823d0ee0d3ace3cf7
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-08 23:41:56 -0400

    Time: 2 ms (94.28%), Space: 20.7 MB (97.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,15 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # By sorting based on the start time, we guarantee that 
-        # no interval with an earlier start appears after an interval with a later start.
-        # this will help us merge sequentially in a single pass
-        intervals.sort()
+        # sort by ascending order by start values because this would put
+        # any overlapping intervals next to each other
+        intervals.sort(key=lambda interval: interval[0])
         res = [intervals[0]]
 
         for i in range(1, len(intervals)):
-            if res[-1][1] >= intervals[i][0]:
-                res[-1][1] = max(res[-1][1], intervals[i][1])
+            interval = intervals[i]
+            
+            if interval[0] <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], interval[1])
             else:
-                res.append(intervals[i])
+                res.append(interval)
         
         return res
