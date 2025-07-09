4d171ddb0824778d9c1d5e789378c8472d279049
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-09 00:40:34 -0400

    Time: 7 ms (43.67%), Space: 19 MB (9.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,5 +7,8 @@ class Solution:
 
         for r in range(len(nums)):
             if nums[r] != 0:
+                # perform a swap whenever nums[r] is non-zero
                 nums[l], nums[r] = nums[r], nums[l]
+                # after swap, nums[l] if guaranteed to be non-zero
+                # so only then, increment l ptr
                 l += 1
