ed4d38092f40547812557a28d702cf86a06a55ca
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-09 19:54:08 -0400

    Time: 4 ms (5.74%), Space: 18 MB (12.4%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Input: nums = [2,0,2,1,1,0]
+        # [0, 0, 2, 1, 1, 2], two_ptr = 4, i = 1
+        # [0, 0, 2, 1, 1, 2]
+        # [0, 0, 1, 1, 2, 2]
+
+        # [1, 0, 2, 1, 1, 0], two_ptr = 4, i = 1
+        # [0, 0, 2, 1, 1, 2]
+        # [0, 0, 1, 1, 2, 2]
+
+        # [2, 2, 0, 0]
+        # [0, 2, 0, 2]
+        zero_ptr, two_ptr = 0, len(nums) - 1
+        i = 0
+
+        while i < len(nums):
+            num = nums[i]
+
+            if num == 2 and i < two_ptr:
+                nums[i], nums[two_ptr] = nums[two_ptr], nums[i]
+                two_ptr -= 1
+                print(nums)
+            
+            elif num == 0:
+                nums[i], nums[zero_ptr] = nums[zero_ptr], nums[i]
+                zero_ptr += 1
+                i += 1
+            
+            else:
+                i += 1
