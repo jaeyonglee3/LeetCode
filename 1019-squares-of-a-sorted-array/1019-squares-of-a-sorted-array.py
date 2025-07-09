d816d83c8b397f7578229e5dd5d1f18872cf7ada
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-09 19:19:24 -0400

    Time: 15 ms (25.05%), Space: 19.3 MB (75.45%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -5,22 +5,20 @@ class Solution:
         # if the square of the number at l ptr is greater than the one at r ptr, swap them
         # only increment the left pointer after a swap. if no swap, keep it there
         # right ptr always moves inwards until it meets the left ptr
-
-        res = []
+        res = [0] * len(nums)
         l, r = 0, len(nums) - 1
+        fill_pos = len(nums) - 1
+        
         while r >= l:
             sqr_r, sqr_l = nums[r] ** 2, nums[l] ** 2
 
             if sqr_r > sqr_l:
-                res.append(sqr_r)
+                res[fill_pos] = sqr_r
                 r -= 1
-            elif sqr_r < sqr_l:
-                res.append(sqr_l)
-                l += 1
             else:
-                res.append(sqr_l)
-                if l != r: res.append(sqr_r)
-                r -= 1
+                res[fill_pos] = sqr_l
                 l += 1
+            
+            fill_pos -= 1
         
-        return list(reversed(res))
+        return res
