783228e13c894a93e3a321a76994b0ef865cf407
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-10 00:07:28 -0400

    Time: 576 ms (37.09%), Space: 17.9 MB (59.88%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,59 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # INCORRECT - variable sized sliding window approach
-        # l, r start at index 0
-        # while right is still in range
-        # if string in window not palindrome or length 1, increment right
-        # if string in window is palindrome, increment left, update res
-        # res = ""
-        # left = right = 0
+        # to check if any string is a palindrome, we can start from 
+        # opposite ends and work towards the middle
+        # or we can start from the middle and work outwards 
+        # (some middles are two chars if the palindromic string is of even length)
+        res = ""
+        longest_len = 0
 
-        # while right <= len(s) - 1:
-        #     curr_s = s[left : right + 1]
-        #     print(curr_s)
-
-        #     if curr_s != curr_s[::-1] or len(curr_s) == 1:
-        #         right += 1
-        #     else:
-        #         res = curr_s if len(curr_s) > len(res) else res
-        #         left += 1
-        
-        # return res
-
-        # CORRECT - brute force:
-        # if len(s) == 1:
-        #     return s
-        
-        # res = ""
-
-        # for i in range(len(s)):
-        #     for j in range(i, len(s)):
-        #         substring = s[i : j + 1]
-        #         if substring == substring[::-1]:
-        #             res = substring if len(substring) > len(res) else res
-        
-        # return res
-        
-        # CORRECT - expand from center approach
-        if len(s) <= 1:
-            return s
-
-        def expand_from_center(left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
+        for i in range(len(s)):
+            # treat every character in the string as 
+            # the middle of the potential palindrome
             
-            return s[left + 1:right]
-
-        max_str = s[0]
-
-        for i in range(len(s) - 1):
-            odd = expand_from_center(i, i)
-            even = expand_from_center(i, i + 1)
-
-            if len(odd) > len(max_str):
-                max_str = odd
-            if len(even) > len(max_str):
-                max_str = even
-
-        return max_str
+            # odd length palindrome
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res = s[l : r + 1] if (r - l + 1) > longest_len else res
+                longest_len = max(longest_len, r - l + 1)
+                l -= 1
+                r += 1
+            
+            # even length palindrome
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res = s[l : r + 1] if (r - l + 1) > longest_len else res
+                longest_len = max(longest_len, r - l + 1)
+                l -= 1
+                r += 1
+        
+        return res
