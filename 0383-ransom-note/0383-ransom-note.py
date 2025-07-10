54e8287c92223312bee469fdace61662d940157d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-09 22:36:24 -0400

    Time: 3 ms (98.12%), Space: 17.9 MB (53.65%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,20 +1,10 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # in other words, do ransomNote and magazine strings contain
-        # the same count of each letter?
         if len(ransomNote) > len(magazine):
             return False
+            
+        for c in set(ransomNote):
+            if magazine.count(c) < ransomNote.count(c):
+                return False
 
-        count_r = {}
-        count_m = {}
-
-        for c in ransomNote:
-            count_r[c] = count_r.get(c, 0) + 1
-        
-        for c in magazine:
-            if c not in count_r or count_r.get(c, -1) == count_m.get(c, -2):
-                continue
-
-            count_m[c] = count_m.get(c, 0) + 1
-        
-        return count_r == count_m
+        return True
