82d1e5ceee6f0a0dfe16732db528a21ab56b19aa
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-10 18:00:50 -0400

    Time: 92 ms (93.7%), Space: 18 MB (86.79%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,32 +1,54 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # key insight 1: use a sliding window to go over substrings in s
-        # and find the longest one that can be made of the same letter with k operations
+        # paraphrase and notes
+        # use var length sliding window appraoch
+        # for any substring our window creates, the number of character replacements we need to do
+        # is equal to the total number of non-most-frequent characters in the substring
+        # we can calculate this by taking size of substring MINUS frequency of most-frequent character
 
-        # key insight 2: to calculate the number of operations needed in a window,
-        # use len(current window) minus number of the most frequent character in that window.
+        # maintain a count dictionary containing the count of each char in the substring
+        # also maintain a separate variable that tracks the most frequent character so we can access it quick
 
-        if not s:
-            return 0
-        
+        # if the substring requires more than k replacements, 
+        # move up the left pointer until we need <= k replacements
+
+        # dry rums (examples)
+        # Input: s = "ABAB", k = 2
+        # res = 4
+        # count = {A : 2, B : 2}
+        # most freq = A
+        # substring = A,B,A,B
+
+        # Input: s = "AABABBA", k = 1
+        # res = 4
+        # count = {A : 2, B : 3}
+        # most freq = B
+        # substring = BABBA
+
         res = 1
-        char_freq = {s[0] : 1}
-        most_freq = (s[0], 1)
         l = 0
+        count = {s[l] : 1}
+        most_freq = s[l]
 
         for r in range(1, len(s)):
-            char_freq[s[r]] = char_freq.get(s[r], 0) + 1
+            count[s[r]] = count.get(s[r], 0) + 1
 
-            if char_freq[s[r]] > most_freq[1]:
-                most_freq = (s[r], char_freq[s[r]])
+            if count[s[r]] > count[most_freq]:
+                # we have a new most frequent character
+                most_freq = s[r]
             
-            # calculate the number of operations needed in window
-            num_ops = (r - l + 1) - most_freq[1]
+            num_char_replacements = (r - l + 1) - count[most_freq]
 
-            if num_ops <= k:
-                res = max(res, (r - l + 1))
-            else:
-                char_freq[s[l]] -= 1
+            if num_char_replacements > k:
+                # increment the left ptr once and continue with the loop
+                # we dont increment until num_char_replacements is <= k all at once
+                # because the loop needs to go back to the top incase we have a new most_freq char
+                count[s[l]] -= 1
+                # so by doing this, we effectively shift the window up by exactly 1
+                # without changing the windows size/length
                 l += 1
+            else:
+                # only update the result if we have a valid subarray
+                res = max(res, r - l + 1)
         
         return res
