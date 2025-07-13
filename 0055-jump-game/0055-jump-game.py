9aec1d7cd8650c9689a4f6314be0bf6ef2572b60
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-12 22:44:36 -0400

    Time: 3 ms (99.76%), Space: 18.5 MB (63.27%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,14 +1,14 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # Greedy approach
-        # each num in nums represents the max jump length from that index
+        # greedy solution
+        # work backwards - the goal started at len(nums) - 1
+        # move the goal post back by 1 a bunch of times
+        # at the end, assert that its at index 0
         goal = len(nums) - 1
 
         for i in range(len(nums) - 2, -1, -1):
             if i + nums[i] >= goal:
-                # then, we don't have to get to goal anymore to reach the end
-                # this means we just need to get to i
-                # so i becomes our new goal
+                # move the goal post
                 goal = i
         
         return goal == 0
