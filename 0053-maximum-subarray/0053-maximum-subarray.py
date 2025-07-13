21cab5fdf1e5aad755f5d581788223987cbdbc59
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-13 14:21:49 -0400

    Time: 51 ms (76.59%), Space: 32 MB (66.07%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,14 +1,18 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # greedy solution
+        # a key insight is that the result could never be negative, because if all
+        # the nums are negative, the subarray w/ the largest sum would be [] which has sum 0
+        # so, anytime the curr sum is < 0, reset it back to 0
+
         res = -math.inf
-        curr_sum = 0
+        curr = 0
 
         for n in nums:
-            curr_sum += n
-            res = max(res, curr_sum)
+            curr += n
+            res = max(res, curr)
 
-            if curr_sum < 0:
-                # we must "reset" the curr_sum to start "fresh"
-                curr_sum = 0
+            if curr < 0:
+                curr = 0
         
         return res
