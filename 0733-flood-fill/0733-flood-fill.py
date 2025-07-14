17507d90da874f1ff57f875d7c966fb6a449a669
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-07-14 16:59:56 -0400

    Time: 3 ms (16.21%), Space: 17.9 MB (70.05%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,29 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        # Use a BFS approach - initialize a queue with the starting cell coordinates
-        # at each step of the BFS, popleft from the queue and explore all 4 of that cell's neighbors.
-        # If that cell's neighbors have the SAME color as the starting pixel, update that cell's pixel
-        # value too and add it to the queue to be processed
+        # can solve with BFS
         if image[sr][sc] == color: return image
 
-        ROWS, COLS = len(image), len(image[0])
         DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        ROWS, COLS = len(image), len(image[0])
+        og_color = image[sr][sc]
+
+        def dfs(r, c) -> None:
+            if min(r, c) < 0 or r == ROWS or c == COLS or image[r][c] != og_color:
+                return
+            
+            image[r][c] = color
+            for dr, dc in DIRS:
+                nr, nc = r + dr, c + dc
+                dfs(nr, nc)
         
-        og_pixel = image[sr][sc]
-        q = collections.deque()
-        q.append((sr, sc))
-
-        while q:
-            for _ in range(len(q)):
-                r, c = q.popleft()
-                image[r][c] = color
-
-                for dr, dc in DIRS:
-                    nr, nc = r + dr, c + dc
-
-                    if min(nr, nc) < 0 or nr == ROWS or nc == COLS or image[nr][nc] != og_pixel:
-                        continue
-                        
-                    q.append((nr, nc))
-        
+        dfs(sr, sc)
         return image
