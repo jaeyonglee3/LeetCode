ca51359336a00d828d1b8d295058123be0712f60
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-10-14 23:49:11 -0400

    Time: 576 ms (58.17%), Space: 20.7 MB (70.94%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,45 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # paraphrase
-        # given an array nums, return a list of lists, containing all groups of 3
-        # numbers from the array that sum to 0
-        # no duplicates in groups of 3, and same element can't be used more than once in a group
+        # we need 3 numbers x, y, z such that the sum of them is 0
+        # let i, j, k be the indices of x, y, z respectively
+        
+        # 1. sort the input array
+        # 2. fix x as one number
+        # 3. perform two sum with sorted input array on the remaining part of the arr (target = -x)
+        nums.sort()
 
-        # dry runs (examples)
-        # nums = [-1,0,1,2,-1,-4]
-        # sort_nums = [-4, 2, 2, 2, 2, 5, 5, 5]
-        # result = [[-1, 0, -1], [-1, -1, 2]]
-        # nums[i] = -1
-        # nums[j] = -1
-        # nums[k] = 2
-
-        # approach
-        # fix the nums[i] number first
-        # then, search the rest of the array for two numbers that add up to the negation of nums[i]
-        # we can do this using two pointers, 
-        # moving them depending on if the sum at the moment if too large or too little
-        # add the list of [nums[i], nums[j], nums[k]] to the result
-
-        # implement
-        nums.sort()  # O(n log n) operation
         res = []
-
-        for i, num_i in enumerate(nums):
-            if i > 0 and nums[i - 1] == num_i:
+        for i, x in enumerate(nums):
+            if i > 0 and x == nums[i - 1]:
                 continue
-                
-            target = -num_i
-            l, r = i + 1, len(nums) - 1
 
+            target = -1 * x
+
+            # perform 2 sum from index i + 1 to the end
+            l, r = i + 1, len(nums) - 1
             while r > l:
-                curr_sum = nums[l] + nums[r]
+                y, z = nums[l], nums[r]
+                curr_sum = y + z
 
                 if curr_sum == target:
-                    res.append([num_i, nums[l], nums[r]])
-                    r -= 1
+                    res.append([x, y, z])
                     l += 1
+                    r -= 1
                     while r > 0 and nums[r] == nums[r + 1]:
-                        r-= 1
+                        r -= 1
                     while l < len(nums) and nums[l] == nums[l - 1]:
                         l += 1
                 elif curr_sum > target:
@@ -48,3 +35,5 @@ class Solution:
                     l += 1
         
         return res
+
+        # [-4, 2, 2, 2, 2]
