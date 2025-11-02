92a4288ff0bce3cde92c67849df13a6aad38e99d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-11-02 16:36:38 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # DFS approach:
-        # iterate over the entire grid. Any time you encounter an island, increment the total 
-        # number of islands. Then, start a DFS search from that island to turn it and all
-        # connected 1s into 0s so that we don't count the same island more than once.
-
-        res = 0
+        # BFS approach: visit every cell. If the cell is an island,
+        # we will use BFS to mark it and every connected "1" as "0"
+        # so that we don't count that same island twice
         ROWS, COLS = len(grid), len(grid[0])
         DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        res = 0
+        
+        def bfs(r, c):
+            q = collections.deque()
+            q.append((r, c))
 
-        def dfs(r, c):
-            if min(r, c) < 0 or r == ROWS or c == COLS or grid[r][c] == "0":
-                return
-            
-            grid[r][c] = "0"
-            for dr, dc in DIRS:
-                dfs(r + dr, c + dc)
+            while q:
+                for _ in range(len(q)):
+                    r, c = q.popleft()
+                    grid[r][c] = "0"
 
+                    for dr, dc in DIRS:
+                        nr, nc = r + dr, c + dc
+                        if min(nr, nc) < 0 or nr == ROWS or nc == COLS or grid[nr][nc] == "0":
+                            continue
+                        q.append((nr, nc))
+             
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    dfs(r, c)
+                    bfs(r, c)
                     res += 1
         
         return res
