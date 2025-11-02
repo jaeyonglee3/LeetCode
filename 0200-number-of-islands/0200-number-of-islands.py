dac3a3b145ec1aa1f083f22a01b634b730d3eb00
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2025-11-02 16:36:50 -0500

    Time: 263 ms (36.17%), Space: 20.4 MB (42.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # BFS approach: visit every cell. If the cell is an island,
-        # we will use BFS to mark it and every connected "1" as "0"
+        # DFS approach: visit every cell. If the cell is an island,
+        # DFS starting from that cell and mark each connected "1" as "0"
         # so that we don't count that same island twice
         ROWS, COLS = len(grid), len(grid[0])
         DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         res = 0
         
-        def bfs(r, c):
-            q = collections.deque()
-            q.append((r, c))
-
-            while q:
-                for _ in range(len(q)):
-                    r, c = q.popleft()
-                    grid[r][c] = "0"
-
-                    for dr, dc in DIRS:
-                        nr, nc = r + dr, c + dc
-                        if min(nr, nc) < 0 or nr == ROWS or nc == COLS or grid[nr][nc] == "0":
-                            continue
-                        q.append((nr, nc))
-             
+        def dfs(r, c):
+            if min(r, c) < 0 or r == ROWS or c == COLS or grid[r][c] == "0":
+                return
+            
+            # mark this current cell as water
+            grid[r][c] = "0"
+            for dr, dc in DIRS:
+                dfs(r + dr, c + dc)
+            
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    bfs(r, c)
+                    dfs(r, c)
                     res += 1
         
         return res
