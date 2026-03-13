b5a874787ab02e55816d6f7963952076dc190e5c
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2026-03-13 16:11:30 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,18 +1,31 @@
 class Solution:
     def reverse(self, x: int) -> int:
         # -2^31 and 2^31 - 1
+        MIN = -2147483648
+        MAX = 2147483647
+        
+        is_neg = x < 0
+        x = abs(x)
+        res = 0
 
-        str_x = str(x)
-        if x < 0:
-            str_x = str_x[1: ]
-        
-        str_x = str_x[::-1]
-        result = int(str_x)
+        while x != 0:
+            digit = x % 10
+            x = x // 10
 
-        if x < 0:
-            result *= -1
+            # Ensure no overflow (no value outside 32-bit integer)
+            max_last_digit = MAX % 10  # 7
+            min_last_digit = MIN % 10  # 8
+            
+            if (res > MAX // 10 or res == MAX // 10 and digit >= MAX % 10):
+                # result too big
+                return 0
+            
+            if (res < MIN // 10 or res == MIN // 10 and digit >= MIN % 10):
+                # result too small
+                return 0
+
+            # Now, update the digit
+            res *= 10
+            res += digit
         
-        if result < -1 * (2 ** 31) or result > (2 ** 31) + 1:
-            return 0
-        
-        return result
+        return -1 * res if is_neg else res
