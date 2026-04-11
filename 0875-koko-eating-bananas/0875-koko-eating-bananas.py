e8b6bea43dd506e738fd7e10aadc7ab7961721f9
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2026-04-11 00:04:15 -0400

    Time: 187 ms (29.87%), Space: 20.5 MB (86.06%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,29 +1,35 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # binary search approach
-        def feasible(speed: int) -> bool:
-            hrs = 0
-            
+        # want to find the smallest k such that we can 
+        # finish all bananas under h hours
+        # the smallest possible k? - 1
+        # the largest possible k? - max(piles)
+
+        res = 0
+
+        def is_valid(k) -> bool:
+            time = 0
+
             for pile in piles:
-                if pile < speed:
-                    hrs += 1
-                else:
-                    hrs += math.ceil(pile/speed)
+                if pile < k:
+                    time += 1
+                    continue
                 
-                if hrs > h:
-                    return False
-            
-            return True
-        
-        # define a search boundary
-        left, right = 1, max(piles)
-        while left < right:
-            mid = (left + right) // 2
-            
-            if feasible(mid):
-                right = mid
+                time += math.ceil(pile / k)
+
+            return time <= h
+
+        l, r = 1, max(piles)
+        while r >= l:
+            candidate = (r + l) // 2
+
+            if is_valid(candidate):
+                # this is "good enough"
+                # but can we do better by eating slower?
+                res = candidate
+                r = candidate - 1
             else:
-                left = mid + 1
+                # otherwise, we need to eat faster
+                l = candidate + 1
         
-        return left
-        
+        return res
