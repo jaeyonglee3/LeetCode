8928c73734a3118bd37442c00ab5f2c0f89e784d
Jaeyong Lee <73498725+jaeyonglee3@users.noreply.github.com>
2026-05-30 12:33:19 -0400

    Time: 18 ms (36.96%), Space: 28.5 MB (72.62%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,52 +1,54 @@
 class Twitter:
 
     def __init__(self):
-        self.count = -1
-        self.tweet_map = defaultdict(list)  # userId -> list of [(count, tweetIds)]
-        self.follow_map = defaultdict(set)  # userId -> set of followeeId
+        self.timestamp = 0
+        self.tweets = collections.defaultdict(list)  # userId : tweets list
+        self.following = collections.defaultdict(set)  # userId : following userIds
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweet_map[userId].append((self.count, tweetId))
-        self.count -= 1
+        # tweets are tuples: (timestamp, tweetId)
+        new_tweet = (-self.timestamp, tweetId)
+        self.timestamp += 1
+        self.tweets[userId].append(new_tweet)
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        res = []
-        min_heap = []
-
-        self.follow_map[userId].add(userId)  # make the user follow themself
-        for uid in self.follow_map[userId]:
-            if uid in self.tweet_map:
-                # only get uid's most recent tweet
-                index = len(self.tweet_map[uid]) - 1
-                count, tweetId = self.tweet_map[uid][index]
-
-                # push 4 values, the count, tweetid, uid of person who posted it, and next index
-                heapq.heappush(min_heap, [count, tweetId, uid, index - 1])
+        # pull the latest tweet from each user userId follows (incl themself)
+        # into a max heap and heappop the latest 10
+        result = []
+        tweet_heap = []
+        curr_tweet_index = -1
         
-        while min_heap and len(res) < 10:
-            # stops if min_heap is empty, or if len(res) >= 10, whichever happens first
-            count, tweetId, uid, index = heapq.heappop(min_heap)
-            res.append(tweetId)
+        while len(result) < 10:
+            users_set = self.following[userId]
+            users_set.add(userId)
 
-            if index >= 0:
-                # that index tells us the next tweet belonging to uid that we can add
-                # to the feed. add it to the heap using heappush
-                count, tweetId = self.tweet_map[uid][index]
-                heapq.heappush(min_heap, [count, tweetId, uid, index - 1])
+            for user in self.following[userId]:
+                user_tweets = self.tweets[user]
+                if len(user_tweets) < abs(curr_tweet_index):
+                    continue
+                
+                curr_tweet = user_tweets[curr_tweet_index]
+                heapq.heappush(tweet_heap, curr_tweet)
+            
+            curr_tweet_index -= 1
+            if not tweet_heap:
+                return result
+            
+            result.append(heapq.heappop(tweet_heap)[1])
         
-        return res
+        return result
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.follow_map[followerId].add(followeeId)
+        self.following[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.follow_map[followerId]:
-            self.follow_map[followerId].remove(followeeId)
-        
+        if followeeId in self.following[followerId]:
+            self.following[followerId].remove(followeeId)
+
 
 # Your Twitter object will be instantiated and called as such:
 # obj = Twitter()
 # obj.postTweet(userId,tweetId)
-# param_2 = obj.getnews_feed(userId)
+# param_2 = obj.getNewsFeed(userId)
 # obj.follow(followerId,followeeId)
 # obj.unfollow(followerId,followeeId)
